6dcdd8d41c34ad3742cf6d90f262d8747fa6caa5
Denis Buserski <81383938+DenisBuserski@users.noreply.github.com>
2022-02-28 08:27:10 +0200

    Add files via upload

---
+++ Programming_Fundamentals_Mid_Exam_Retake_6_August_2019/01_Black_Flag.java
@@ -0,0 +1,33 @@
+package ProgrammingFundamentalsMidExamRetake6August2019;
+
+import java.util.Scanner;
+
+public class BlackFlag {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int days = Integer.parseInt(scanner.nextLine());
+        int dailyPlunder = Integer.parseInt(scanner.nextLine());
+        double target = Double.parseDouble(scanner.nextLine());
+
+        double gatheredPlunder = 0;
+        for (int i = 1; i <= days; i++) {
+            gatheredPlunder += dailyPlunder;
+
+            if (i % 3 == 0) {
+                gatheredPlunder += 0.5 * dailyPlunder;
+            }
+            if (i % 5 == 0) {
+                gatheredPlunder = 0.7 * gatheredPlunder;
+            }
+        }
+
+        if (gatheredPlunder >= target) {
+            System.out.printf("Ahoy! %.2f plunder gained.", gatheredPlunder);
+        } else {
+            System.out.printf("Collected only %.2f%% of the plunder.", (gatheredPlunder / target) * 100);
+        }
+
+
+    }
+}
