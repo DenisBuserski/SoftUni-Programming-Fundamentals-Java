751025acceb566523eefe913ce774576742c4d21
Denis Buserski <81383938+DenisBuserski@users.noreply.github.com>
2022-03-04 19:11:28 +0200

    Update Post_Office_03.java

--- Regular_Expressions_More_Exercise/Post_Office_03.java
+++ Regular_Expressions_More_Exercise/Post_Office_03.java
@@ -1,11 +1,9 @@
-package RegularExpressionsMoreExercise;
-
 import java.util.LinkedHashMap;
 import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class PostOffice {
+public class Post_Office_03 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
@@ -14,7 +12,7 @@ public class PostOffice {
         String secondPart = input[1];
         String thirdPart = input[2];
         LinkedHashMap<Character, Integer> letters = new LinkedHashMap<>();
-        //малко съм ти преправил патърна, номисля, че и твоя вървеше правилно
+       
         Pattern patternText = Pattern.compile("([#$%*&])([A-Z]+)\\1");
         Matcher matcherText = patternText.matcher(firstPart);
 
@@ -24,13 +22,9 @@ public class PostOffice {
             capitalLetters = current.substring(1, current.length() - 1);
         }
 
-
-        //([65-90]{1,2})\:([0-9]{2})
-
         for (int i = 0; i < capitalLetters.length(); i++) {
             char symbol = capitalLetters.charAt(i);
 
-            // патърна съм го вкарал в лупа, за да се инициализира на ново всеки път!
             Pattern patternDigits = Pattern.compile("([0-9]{2}):([0-9]{2})");
             Matcher matcherDigits = patternDigits.matcher(secondPart);
 
@@ -40,8 +34,6 @@ public class PostOffice {
                 int asciiCode = Integer.parseInt(digits[0]);
                 int length = Integer.parseInt(digits[1]);
 
-                //добавил съм и едно условие , да не презаписва дължината на думата, ако вече сме записали ентри с този ключ
-                // (в противен случай ще ъпдейтне стойноста с последната намерена, а на нас ни трябва първата намерена
                 if (symbol == asciiCode && !letters.containsKey(symbol)) {
                     letters.put(symbol, length + 1);
                     break;
@@ -51,12 +43,10 @@ public class PostOffice {
 
         String[] thirdText = thirdPart.split("\\s+");
         for (int i = 0; i < capitalLetters.length(); i++) {
-
             char firstLetter = capitalLetters.charAt(i);
             int length = letters.get(firstLetter);
 
             for (String word : thirdText) {
-
                 int lengthWord = word.length();
                 String wordFirstChar = word.substring(0, 1);
                 char firstChar = wordFirstChar.charAt(0);
@@ -65,9 +55,7 @@ public class PostOffice {
                     System.out.println(word);
                 }
             }
-
         }
-
-
+        
     }
 }
