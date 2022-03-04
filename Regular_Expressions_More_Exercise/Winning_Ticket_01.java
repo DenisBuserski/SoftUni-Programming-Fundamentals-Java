5d2e8ad70efd1b6ae681fb38e611aa8b988aaedb
Denis Buserski <81383938+DenisBuserski@users.noreply.github.com>
2022-03-04 19:00:24 +0200

    Update Winning_Ticket_01.java

--- Regular_Expressions_More_Exercise/Winning_Ticket_01.java
+++ Regular_Expressions_More_Exercise/Winning_Ticket_01.java
@@ -1,11 +1,30 @@
-package RegularExpressionsMoreExercise;
-
 import java.util.Scanner;
 
-public class WinningTicket {
+public class Winning_Ticket_01 {
+    
+    private static final String REGEX = "(?=.{20}).*?(?=(?<ch>[@#$^]))(?<match>\\k<ch>{6,}).*(?<=.{10})\\k<match>.*";
+    private static final Pattern PATTERN = Pattern.compile(REGEX);
+    private static final Pattern SEPARATOR = Pattern.compile("\\s*,\\s*");
+   
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+       
+        String[] tickets = SEPARATOR.split(scanner.nextLine().trim());
 
-
+        for (String ticket : tickets) {
+            if (ticket.length() != 20) {
+                System.out.println("invalid ticket");
+            } else {
+                Matcher matcher = PATTERN.matcher(ticket);
+                if (matcher.matches()) {
+                    String match = matcher.group("match");
+                    System.out.printf("ticket \"%s\" - %d%s%s%n",
+                            ticket, match.length(), match.charAt(0),
+                            (match.length() == 10) ? " Jackpot!" : "");
+                } else {
+                    System.out.printf("ticket \"%s\" - no match%n", ticket);
+                }
+            }
+        }
     }
 }
