8425abfdf63c4da33dac0de9492720a378476d75
Denis Buserski <81383938+DenisBuserski@users.noreply.github.com>
2022-03-05 11:57:52 +0200

    Update Car_Race_02.java

--- Lists_More_Exercise/Car_Race_02.java
+++ Lists_More_Exercise/Car_Race_02.java
@@ -1,20 +1,18 @@
-package ListsMoreExercise;
-
 import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 import java.util.stream.Collectors;
 
-public class CarRace {
+public class Car_Race_02 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
         List<Integer> times = Arrays.stream(scanner.nextLine().split(" ")).map(Integer::parseInt).collect(Collectors.toList());
 
         int finishTime = times.size() / 2;
-
         double timeFirstCar = 0;
         double timeSecondCar = 0;
+        
         for (int index = 0; index < finishTime; index++) {
             int time = times.get(index);
             if (time == 0) {
@@ -37,7 +35,6 @@ public class CarRace {
         } else {
             System.out.printf("The winner is right with total time: %.1f", timeSecondCar);
         }
-
 
     }
 }
