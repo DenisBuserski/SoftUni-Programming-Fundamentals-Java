c4cd36e17e0f7082fcdd665dfe549e8fd1adeb0c
Denis Buserski <81383938+DenisBuserski@users.noreply.github.com>
2022-03-07 09:56:05 +0200

    Update Merging_Lists_03.java

--- Lists_Lab/Merging_Lists_03.java
+++ Lists_Lab/Merging_Lists_03.java
@@ -1,11 +1,9 @@
-package ListsLab;
-
 import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 import java.util.stream.Collectors;
 
-public class MergingLists {
+public class Merging_Lists_03 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
