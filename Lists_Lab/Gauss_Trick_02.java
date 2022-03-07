53ced4aef7556f18e0c0f1c02f9b0d2a6451062c
Denis Buserski <81383938+DenisBuserski@users.noreply.github.com>
2022-03-07 09:51:24 +0200

    Update Gauss_Trick_02.java

--- Lists_Lab/Gauss_Trick_02.java
+++ Lists_Lab/Gauss_Trick_02.java
@@ -1,11 +1,9 @@
-package ListsLab;
-
 import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 import java.util.stream.Collectors;
 
-public class GaussTrick {
+public class Gauss_Trick_02 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
