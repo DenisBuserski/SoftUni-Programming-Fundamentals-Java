5bc3a7224d9d59ef343a8008b870c2da3ff8c581
Denis Buserski <81383938+DenisBuserski@users.noreply.github.com>
2022-03-08 08:12:50 +0200

    Add files via upload

---
+++ Lists_Lab/List_Of_Products_06.java
@@ -0,0 +1,28 @@
+package ListsLab;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+
+public class ListOfProducts {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = Integer.parseInt(scanner.nextLine());
+
+        List<String> products = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            products.add(scanner.nextLine());
+        }
+
+        Collections.sort(products);
+
+        for (int i = 0; i < products.size(); i++) {
+            System.out.printf("%d.%s%n", i + 1, products.get(i));
+        }
+
+
+    }
+}
