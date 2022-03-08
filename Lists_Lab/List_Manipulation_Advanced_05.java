6c8d123f7fca04a1342492c537c5147bef75b4b4
Denis Buserski <81383938+DenisBuserski@users.noreply.github.com>
2022-03-08 08:08:10 +0200

    Update and rename List_Manipulation_Advanced_04.java to List_Manipulation_Advanced_05.java

--- Lists_Lab/List_Manipulation_Advanced_05.java
+++ Lists_Lab/List_Manipulation_Advanced_05.java
@@ -3,7 +3,7 @@ import java.util.List;
 import java.util.Scanner;
 import java.util.stream.Collectors;
 
-public class List_Manipulation_Advanced_04 {
+public class List_Manipulation_Advanced_05 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
