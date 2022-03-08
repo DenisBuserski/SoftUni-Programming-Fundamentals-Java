d4f4bda7659ee11c9a9ef00ede0404c1fbf0d0f6
Denis Buserski <81383938+DenisBuserski@users.noreply.github.com>
2022-03-08 08:13:35 +0200

    Update List_Of_Products_06.java

--- Lists_Lab/List_Of_Products_06.java
+++ Lists_Lab/List_Of_Products_06.java
@@ -1,11 +1,9 @@
-package ListsLab;
-
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Scanner;
 
-public class ListOfProducts {
+public class List_Of_Products_06 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
@@ -22,7 +20,5 @@ public class ListOfProducts {
         for (int i = 0; i < products.size(); i++) {
             System.out.printf("%d.%s%n", i + 1, products.get(i));
         }
-
-
     }
 }
