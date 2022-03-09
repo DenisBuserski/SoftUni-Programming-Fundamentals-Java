cf35b014063f428c609a24a44444da262402ebce
Denis Buserski <81383938+DenisBuserski@users.noreply.github.com>
2022-03-09 09:42:37 +0200

    Update Train_01.java

--- Lists_Exercise/Train_01.java
+++ Lists_Exercise/Train_01.java
@@ -1,11 +1,9 @@
-package ListsExercise;
-
 import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 import java.util.stream.Collectors;
 
-public class Train {
+public class Train_01 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
