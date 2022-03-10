46c7e5d2e3ef04d17f109ea0c8a78497b52b6bb9
Denis Buserski <81383938+DenisBuserski@users.noreply.github.com>
2022-03-10 15:19:46 +0200

    Add files via upload

---
+++ Lists_Exercise/Bomb_Numbers_05.java
@@ -0,0 +1,37 @@
+package ListsExercise;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class BombNumbers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        List<Integer> numbers = Arrays.stream(scanner.nextLine().split("\\s+")).map(Integer::parseInt).collect(Collectors.toList());
+
+        String[] input = scanner.nextLine().split("\\s+");
+        int bombNumber = Integer.parseInt(input[0]);
+        int bombPower = Integer.parseInt(input[1]);
+
+        int sum = 0;
+
+        while (numbers.contains(bombNumber)) {
+            int indexBomb = numbers.indexOf(bombNumber);
+
+            int left = Math.max(0, indexBomb - bombPower);
+            int right = Math.min(numbers.size() - 1, indexBomb + bombPower);
+
+            for (int i = right; i >= left; i--) {
+                numbers.remove(i);
+            }
+        }
+
+        for (int i = 0; i < numbers.size(); i++) {
+            sum += numbers.get(i);
+        }
+        System.out.println(sum);
+
+    }
+}
