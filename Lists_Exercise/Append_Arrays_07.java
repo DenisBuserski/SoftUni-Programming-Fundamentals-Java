8c783557897d6f9045d6b429de369af532ce458a
Denis Buserski <81383938+DenisBuserski@users.noreply.github.com>
2022-03-11 11:20:37 +0200

    Add files via upload

---
+++ Lists_Exercise/Append_Arrays_07.java
@@ -0,0 +1,29 @@
+package ListsExercise;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class AppendArrays {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        List<String> input = Arrays.stream(scanner.nextLine().split("\\|")).collect(Collectors.toList());
+
+        List<String> output = new ArrayList<>();
+
+        for (int i = input.size() - 1; i >= 0; i--) {
+            String[] numbers = input.get(i).split("\\s+");
+            for (int j = 0; j < numbers.length; j++) {
+                if (!numbers[j].equals("")) {
+                    output.add(numbers[j]);
+                }
+            }
+        }
+
+        String print = output.toString().trim();
+        System.out.println(print.replaceAll("\\[|,|\\]", ""));
+    }
+}
