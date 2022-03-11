a964dd31dd295c201687c8afc00dee861d65fb1e
Denis Buserski <81383938+DenisBuserski@users.noreply.github.com>
2022-03-11 11:21:17 +0200

    Update Append_Arrays_07.java

--- Lists_Exercise/Append_Arrays_07.java
+++ Lists_Exercise/Append_Arrays_07.java
@@ -1,17 +1,14 @@
-package ListsExercise;
-
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 import java.util.stream.Collectors;
 
-public class AppendArrays {
+public class Append_Arrays_07 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
         List<String> input = Arrays.stream(scanner.nextLine().split("\\|")).collect(Collectors.toList());
-
         List<String> output = new ArrayList<>();
 
         for (int i = input.size() - 1; i >= 0; i--) {
