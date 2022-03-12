83f07ac6acba39de5931ea55d3da846c783bf7a1
Denis Buserski <81383938+DenisBuserski@users.noreply.github.com>
2022-03-12 08:34:42 +0200

    Add files via upload

---
+++ Text_Processing_Lab/Reverse_Strings_01.java
@@ -0,0 +1,23 @@
+package TextProcessingLab;
+
+import java.util.Scanner;
+
+public class ReverseStrings {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String input = scanner.nextLine();
+
+        while (!input.equals("end")) {
+            String reversed = "";
+
+            for (int i = input.length() - 1; i >= 0; i--) {
+                reversed += input.charAt(i);
+            }
+            System.out.printf("%s = %s%n", input, reversed);
+
+
+            input = scanner.nextLine();
+        }
+    }
+}
