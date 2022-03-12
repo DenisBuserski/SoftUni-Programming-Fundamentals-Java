9c7f20f233dd0eedcf399aeb4f5107c349e7f332
Denis Buserski <81383938+DenisBuserski@users.noreply.github.com>
2022-03-12 08:37:05 +0200

    Update Repeat_Strings_02.java

--- Text_Processing_Lab/Repeat_Strings_02.java
+++ Text_Processing_Lab/Repeat_Strings_02.java
@@ -1,18 +1,14 @@
-package TextProcessingLab;
-
 import java.util.Scanner;
 
-public class RepeatStrings {
+public class Repeat_Strings_03 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
         String[] words = scanner.nextLine().split(" ");
-
         for (String word : words) {
             String repetedWord = repeatWord(word, word.length());
             System.out.print(repetedWord);
         }
-
     }
 
     private static String repeatWord(String word, int numberOfRepetitions) {
@@ -25,4 +21,3 @@ public class RepeatStrings {
     }
 
 }
-
