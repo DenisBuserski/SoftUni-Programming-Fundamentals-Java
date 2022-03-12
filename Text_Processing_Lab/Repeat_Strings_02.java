8955d22c225105178cb3802f27bb744cd398edf4
Denis Buserski <81383938+DenisBuserski@users.noreply.github.com>
2022-03-12 08:37:25 +0200

    Update Repeat_Strings_02.java

--- Text_Processing_Lab/Repeat_Strings_02.java
+++ Text_Processing_Lab/Repeat_Strings_02.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Repeat_Strings_03 {
+public class Repeat_Strings_02 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
