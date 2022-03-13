62514a4c5cb1c6ade69402b487f7c9befee1b9cd
Denis Buserski <81383938+DenisBuserski@users.noreply.github.com>
2022-03-13 19:34:33 +0200

    Update Digits_Letters_And_Other_05.java

--- Text_Processing_Lab/Digits_Letters_And_Other_05.java
+++ Text_Processing_Lab/Digits_Letters_And_Other_05.java
@@ -1,8 +1,6 @@
-package TextProcessingLab;
-
 import java.util.Scanner;
 
-public class DigitsLettersAndOther {
+public class Digits_Letters_And_Other_05 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
