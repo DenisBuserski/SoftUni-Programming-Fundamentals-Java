bbcce2c6ca1d0246747a54e282cca8d366e0fac9
Denis Buserski <81383938+DenisBuserski@users.noreply.github.com>
2022-03-13 19:31:15 +0200

    Add files via upload

---
+++ Text_Processing_Lab/Substring_03.java
@@ -0,0 +1,26 @@
+package TextProcessingLab;
+
+import java.util.Scanner;
+
+public class Substring {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String wordToRemove = scanner.nextLine();
+        String input = scanner.nextLine();
+
+        while (input.contains(wordToRemove)) {
+            input = removeOccurrence(input, wordToRemove);
+
+        }
+        System.out.println(input);
+    }
+
+    private static String removeOccurrence(String input, String wordToRemove) {
+        int beginIndex = input.indexOf(wordToRemove);
+        int endIndex = beginIndex + wordToRemove.length();
+
+        String result = input.substring(0, beginIndex) + (input.substring(endIndex));
+        return result;
+    }
+}
