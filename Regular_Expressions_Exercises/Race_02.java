b982f43e489b31ecb55fa9d5aa1aa8a739a330c4
Denis Buserski <81383938+DenisBuserski@users.noreply.github.com>
2022-03-14 08:19:32 +0200

    Update Race_02.java

--- Regular_Expressions_Exercises/Race_02.java
+++ Regular_Expressions_Exercises/Race_02.java
@@ -1,11 +1,9 @@
-package RegularExpressionsExercises;
-
 import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
-public class Race {
+public class Race_02 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
