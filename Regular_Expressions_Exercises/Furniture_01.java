11da76a9c5b9ed6ba071979b8807a0958f1885e0
Denis Buserski <81383938+DenisBuserski@users.noreply.github.com>
2022-03-14 08:17:11 +0200

    Add files via upload

---
+++ Regular_Expressions_Exercises/Furniture_01.java
@@ -0,0 +1,37 @@
+package RegularExpressionsExercises;
+
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Furniture {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String regex = ">>(?<furniture>\\w+)<<(?<prise>\\d+.\\d*)!(?<quantity>\\d+)";
+        String input = scanner.nextLine();
+
+        System.out.println("Bought furniture:");
+        double result = 0;
+
+        while (!input.equals("Purchase")) {
+            Pattern pattern = Pattern.compile(regex);
+            Matcher matcher = pattern.matcher(input);
+
+            while (matcher.find()) {
+                String furniture = matcher.group("furniture");
+                String prise = matcher.group("prise");
+                double priseNum = Double.parseDouble(prise);
+                String quantity = matcher.group("quantity");
+                double quantityNum = Double.parseDouble(quantity);
+                result += priseNum * quantityNum;
+
+                System.out.printf("%s%n", furniture);
+            }
+
+            input = scanner.nextLine();
+        }
+
+        System.out.printf("Total money spend: %.2f", result);
+    }
+}
