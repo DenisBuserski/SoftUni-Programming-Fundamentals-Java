b986a649d9ce73edf348d44791025793108bb9b6
Denis Buserski <81383938+DenisBuserski@users.noreply.github.com>
2022-03-16 09:05:50 +0200

    Add files via upload

---
+++ Regular_Expressions_Exercises/Extract_Emails_06.java
@@ -0,0 +1,23 @@
+package RegularExpressionsExercises;
+
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class ExtractEmails {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String regexEmail = "[A-Za-z0-9]+[._-]?[A-Za-z0-9]+@[A-Za-z]+-?[A-Za-z]+(\\.[A-Za-z]+-?[A-Za-z])+";
+        Pattern pattern = Pattern.compile(regexEmail);
+        String input = scanner.nextLine();
+
+        Matcher matcher = pattern.matcher(input);
+
+        while(matcher.find()) {
+            System.out.println(matcher.group());
+        }
+
+
+    }
+}
