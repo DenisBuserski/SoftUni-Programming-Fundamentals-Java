4f289ddd88290e6b5fc0f9bb90ccda932622b45a
Denis Buserski <81383938+DenisBuserski@users.noreply.github.com>
2022-03-16 09:05:18 +0200

    Update Nether_Realms_05.java

--- Regular_Expressions_Exercises/Nether_Realms_05.java
+++ Regular_Expressions_Exercises/Nether_Realms_05.java
@@ -1,12 +1,10 @@
-package RegularExpressionsExercises;
-
 import java.text.DecimalFormat;
 import java.text.NumberFormat;
 import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class NetherRealms {
+public class Nether_Realms_05 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
@@ -14,7 +12,6 @@ public class NetherRealms {
         String[] inputArray = input.split("\\s*\\,+\\s*");
 
         Map<String, List<String>> allData = new TreeMap<>();
-        //heroData.put(heroName, Arrays.asList(heroHP, heroMP));
 
         for (int i = 0; i <= inputArray.length - 1; i++) {
             String demonInfo = inputArray[i];
@@ -22,6 +19,7 @@ public class NetherRealms {
             String validRegex = "(?<valid>[^\\,\\s+]{1,})";
             Pattern validPattern = Pattern.compile(validRegex);
             Matcher validMatcher = validPattern.matcher(demonInfo);
+            
             if (validMatcher.find()) {
 
                 StringBuilder health = new StringBuilder();
@@ -42,13 +40,16 @@ public class NetherRealms {
                 String baseDamageRegex = "(?<damage>\\-*[0-9]+\\.*[0-9]*)";
                 Pattern baseDamagePattern = Pattern.compile(baseDamageRegex);
                 Matcher baseDamageMatcher = baseDamagePattern.matcher(demonInfo);
+                
                 while (baseDamageMatcher.find()) {
                     baseDamage.add(baseDamageMatcher.group("damage"));
                 }
+                
                 for (int index = 0; index <= baseDamage.size() - 1; index++) {
                     double number = Double.parseDouble(baseDamage.get(index));
                     damageRate += number;
                 }
+                
                 for (int index = 0; index <= demonInfo.length() - 1; index++) {
                     if (demonInfo.charAt(index) == '*') {
                         damageRate *= 2;
@@ -56,6 +57,7 @@ public class NetherRealms {
                         damageRate /= 2;
                     }
                 }
+                
                 NumberFormat formatter = new DecimalFormat("#0.00");
                 String newDamageRate = formatter.format(damageRate);
                 String healthString = Integer.toString(healthRate);
@@ -63,10 +65,14 @@ public class NetherRealms {
                 allData.put(demonInfo, Arrays.asList(healthString, damageString));
             }
         }
-            allData.entrySet()
+            
+        allData.entrySet()
                     .forEach(e -> System.out.printf("%s - %s health, %s damage%n", e.getKey(), e.getValue().get(0), e.getValue().get(1)));
-
-
-
     }
 }
+
+// 1.1!@2.2#$3.999%^0.01&*(0.22)?0.3/* , d0.3r0.2o0.1b0.0, k0.0rab0.02a0.5* , fr0002d0.2*/ 
+// ===================================================== 1.1!@2.2#$3.999%^0.01&*(0.22)?0.3/* - 481 health, 15.66 damage 
+// d0.3r0.2o0.1b0.0 - 423 health, 0.60 damage 
+// fr0002d0.2*/ - 316 health, 2.20 damage 
+// k0.0rab0.02a0.5* - 513 health, 1.04 damage
