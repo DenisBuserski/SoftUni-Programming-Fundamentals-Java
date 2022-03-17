d17599562abb85cadf50d2714cd0b2e62acfb222
Denis Buserski <81383938+DenisBuserski@users.noreply.github.com>
2022-03-17 19:13:30 +0200

    Add files via upload

---
+++ Arrays_More_Exercise/Recursive_Fibonacci_03.java
@@ -0,0 +1,28 @@
+package ArraysMoreExercise;
+
+import java.util.Scanner;
+
+public class RecursiveFibonacci {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = Integer.parseInt(scanner.nextLine());
+
+        long[] fibonacci = new long[n];
+
+        if (n >= 1 && n <= 50) {
+            for (int i = 0; i < n; i++) {
+                if (i == 0 || i == 1) {
+                    fibonacci[i] = 1;
+                } else {
+                    fibonacci[i] = fibonacci[i - 1] + fibonacci[i - 2];
+                }
+            }
+            System.out.println(fibonacci[n - 1]);
+        } else {
+            System.out.println(n);
+
+
+        }
+    }
+}
