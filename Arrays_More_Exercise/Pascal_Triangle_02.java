8b0c8c4e567e1594883dfe3e740fc39aa281a932
Denis Buserski <81383938+DenisBuserski@users.noreply.github.com>
2022-03-17 19:12:55 +0200

    Update Pascal_Triangle_02.java

--- Arrays_More_Exercise/Pascal_Triangle_02.java
+++ Arrays_More_Exercise/Pascal_Triangle_02.java
@@ -1,8 +1,6 @@
-package ArraysMoreExercise;
-
 import java.util.Scanner;
 
-public class PascalTriangle {
+public class Pascal_Triangle_02 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
