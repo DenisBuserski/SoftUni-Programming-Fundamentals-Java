b1c05276279cb01b4db27625e6987344e324f2c7
Denis Buserski <81383938+DenisBuserski@users.noreply.github.com>
2022-03-18 20:16:29 +0200

    Add files via upload

---
+++ Arrays_Exercise/Train_01.java
@@ -0,0 +1,22 @@
+package ArraysExercise;
+
+import java.util.Scanner;
+
+public class Train {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int wagonsCount = Integer.parseInt(scanner.nextLine());
+
+        int passangers = 0;
+        int sumPassangers = 0;
+        for (int i = 1; i <= wagonsCount; i++) {
+            passangers = Integer.parseInt(scanner.nextLine());
+            sumPassangers += passangers;
+            System.out.print(passangers + " ");
+
+        }
+        System.out.println();
+        System.out.println(sumPassangers);
+    }
+}
