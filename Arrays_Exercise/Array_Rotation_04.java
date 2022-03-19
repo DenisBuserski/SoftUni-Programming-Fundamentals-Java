e14458c853199eda3e4852e3612ed160308f0b1d
Denis Buserski <81383938+DenisBuserski@users.noreply.github.com>
2022-03-19 08:10:20 +0200

    Add files via upload

---
+++ Arrays_Exercise/Array_Rotation_04.java
@@ -0,0 +1,26 @@
+package ArraysExercise;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class ArrayRotation {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int[] numbers = Arrays.stream(scanner.nextLine().split(" ")).mapToInt(e -> Integer.parseInt(e)).toArray();
+
+        int countRotations = Integer.parseInt(scanner.nextLine());
+
+        for (int i = 1; i <= countRotations; i++) {
+            int firstNumber = numbers[0];
+
+            for (int index = 0; index < numbers.length - 1; index++) {
+                numbers[index] = numbers[index + 1];
+            }
+            numbers[numbers.length - 1] = firstNumber;
+        }
+        for (int number : numbers) {
+            System.out.print(number + " ");
+        }
+    }
+}
