f9d922020e08056454113ba729f6b338e933c762
Denis Buserski <81383938+DenisBuserski@users.noreply.github.com>
2022-03-19 08:06:32 +0200

    Add files via upload

---
+++ Arrays_Exercise/Common_Elements_02.java
@@ -0,0 +1,27 @@
+package ArraysExercise;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class CommonElements {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String[] words1 = scanner.nextLine().split(" ");
+        String[] words2 = scanner.nextLine().split(" ");
+
+        for (String word2 : words2) {
+            for (String word1 : words1) {
+
+                if (word2.equals(word1)) {
+                    System.out.print(word2 + " ");
+                    break;
+                }
+            }
+        }
+
+
+
+
+    }
+}
