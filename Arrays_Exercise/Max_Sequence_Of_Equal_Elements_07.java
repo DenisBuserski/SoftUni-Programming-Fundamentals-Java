8d7bd00c66b4ae54e9fe3e5b02931cbb9a3954af
Denis Buserski <81383938+DenisBuserski@users.noreply.github.com>
2022-03-20 21:18:22 +0200

    Add files via upload

---
+++ Arrays_Exercise/Max_Sequence_Of_Equal_Elements_07.java
@@ -0,0 +1,46 @@
+package ArraysExercise;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class MaxSequenceOfEqualElements {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int[] array = Arrays.stream(scanner.nextLine().split(" ")).mapToInt(e -> Integer.parseInt(e)).toArray(); // Получавам числата
+        int max = Integer.MIN_VALUE;
+        int maxNumber = Integer.MIN_VALUE;
+        int leftMaxNumber = Integer.MIN_VALUE;
+        int leftMax = Integer.MIN_VALUE;
+
+        int maxSequence = 0;
+
+        for (int i = 0; i < array.length - 1; i++) {
+            int number = array[i]; // Прочитам всяко едно число
+
+            if (number == array[i + 1]) { // Проверявам дали е равно на числото след него
+                maxSequence++;
+                if (maxSequence > max) {
+                    max = maxSequence;
+                    maxNumber = number;
+                }
+                if (max < 0) {
+                    max = leftMax;
+                    leftMaxNumber = number;
+                }
+            } else {
+                maxSequence = 0;
+            }
+        }
+        if (max - leftMax == 0) {
+            for (int i = 1; i <= max + 1; i++) {
+                System.out.print(leftMaxNumber + " ");
+            }
+        } else {
+            for (int i = 1; i <= max + 1; i++) {
+                System.out.print(maxNumber + " ");
+            }
+        }
+    }
+}
+
