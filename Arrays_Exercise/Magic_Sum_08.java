b6c42f1ebf34a53de2a1e1534f732509acdcb3a2
Denis Buserski <81383938+DenisBuserski@users.noreply.github.com>
2022-03-20 21:20:15 +0200

    Add files via upload

---
+++ Arrays_Exercise/Magic_Sum_08.java
@@ -0,0 +1,31 @@
+package ArraysExercise;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class MagicSum {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+        int [] numbers = Arrays.stream(scanner.nextLine().split(" ")).mapToInt(e -> Integer.parseInt(e)).toArray();
+
+        int sum = Integer.parseInt(scanner.nextLine());
+
+        for (int index = 0; index <= numbers.length - 1; index++) {
+            int number = numbers[index];
+
+            for (int i = index + 1; i <= numbers.length - 1; i++) {
+
+                if (index == i) {
+                    continue;
+                }
+                int number2 = numbers[i];
+
+                if (number + number2 == sum) {
+                    System.out.printf("%d %d%n", number, number2);
+                }
+            }
+        }
+    }
+}
