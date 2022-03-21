f00314481903fb2a2fd6152d924f51cb9ba80a27
Denis Buserski <81383938+DenisBuserski@users.noreply.github.com>
2022-03-21 08:50:21 +0200

    Add files via upload

---
+++ Arrays_Exercise/Train_Using_Array_01.java
@@ -0,0 +1,26 @@
+package ArraysExercise;
+
+import java.util.Scanner;
+
+public class TrainUsingArray {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = Integer.parseInt(scanner.nextLine());
+        int[] numbers = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            numbers[i] = Integer.parseInt(scanner.nextLine());
+        }
+
+        int sum = 0;
+        for (int number : numbers) {
+            System.out.println(number + " ");
+            sum += number;
+        }
+
+        System.out.println();
+        System.out.println(sum);
+
+    }
+}
