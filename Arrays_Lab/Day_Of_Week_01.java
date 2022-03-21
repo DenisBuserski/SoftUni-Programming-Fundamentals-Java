71812884cb88a6406e1704cca02d3dd33cac28d4
Denis Buserski <81383938+DenisBuserski@users.noreply.github.com>
2022-03-21 22:37:55 +0200

    Add files via upload

---
+++ Arrays_Lab/Day_Of_Week_01.java
@@ -0,0 +1,30 @@
+package ArraysLab;
+
+import java.util.Scanner;
+
+public class DayOfWeek {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String[] daysOfWeek = {
+                "Monday",
+                "Tuesday",
+                "Wednesday",
+                "Thursday",
+                "Friday",
+                "Saturday",
+                "Sunday"
+        };
+        int currentDay = Integer.parseInt(scanner.nextLine());
+
+        if (currentDay >= 1 && currentDay <= 7) {
+            System.out.println(daysOfWeek[currentDay - 1]);
+        }
+        else {
+            System.out.println("Invalid day!");
+        }
+
+
+
+    }
+}
