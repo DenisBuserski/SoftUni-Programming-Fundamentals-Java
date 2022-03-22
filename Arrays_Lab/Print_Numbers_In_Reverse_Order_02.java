e76726562dea85bb70f46b7d695a24044008d272
Denis Buserski <81383938+DenisBuserski@users.noreply.github.com>
2022-03-22 13:22:46 +0200

    Update Print_Numbers_In_Reverse_Order_02.java

--- Arrays_Lab/Print_Numbers_In_Reverse_Order_02.java
+++ Arrays_Lab/Print_Numbers_In_Reverse_Order_02.java
@@ -1,26 +1,19 @@
-package ArraysLab;
-
 import java.util.Scanner;
 
-public class PrintNumbersInReverseOrder {
+public class Print_Numbers_In_Reverse_Order_02 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
         int n = Integer.parseInt(scanner.nextLine());
-
         int[] numbers = new int[n];
 
         for (int i = 0; i < n; i++) {
             numbers[i] = Integer.parseInt(scanner.nextLine());
-
         }
 
         for (int i = numbers.length - 1; i >= 0; i--) {
-
             System.out.print(numbers[i] + " ");
         }
-
-
 
     }
 }
