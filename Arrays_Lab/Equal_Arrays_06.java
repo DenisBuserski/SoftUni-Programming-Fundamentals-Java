e8323fb6dc7ecd94db9040447a8bc320b88827d7
Denis Buserski <81383938+DenisBuserski@users.noreply.github.com>
2022-03-23 08:32:40 +0200

    Update Equal_Arrays_06.java

--- Arrays_Lab/Equal_Arrays_06.java
+++ Arrays_Lab/Equal_Arrays_06.java
@@ -1,9 +1,7 @@
-package ArraysLab;
-
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class EqualArrays {
+public class Equal_Arrays_06 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
@@ -24,13 +22,13 @@ public class EqualArrays {
 
         for (int i = 0; i < firstArray.length; i++) {
             sum += firstArray[i];
-
             if (firstArray[i] != secondArray[i]) {
                 System.out.printf("Arrays are not identical. Found difference at %d index.", i);
                 areIdentical = false;
                 break;
             }
         }
+        
         if (areIdentical) {
             System.out.printf("Arrays are identical. Sum: %d", sum);
         }
