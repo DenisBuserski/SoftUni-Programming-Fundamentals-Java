716fda69d2be564663ff16a2a548c67268201574
Denis Buserski <81383938+DenisBuserski@users.noreply.github.com>
2022-03-25 08:02:13 +0200

    Add files via upload

---
+++ Data_Types_And_Variables_More_Exercise/Balanced_Brackets_06.java
@@ -0,0 +1,34 @@
+package DataTypesAndVariablesMoreExercise;
+
+import java.util.Scanner;
+
+public class BalancedBrackets {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int nLines = Integer.parseInt(scanner.nextLine());
+        int countIn = 0;
+        int countOut = 0;
+
+        for (int i = 1; i <= nLines; i++) {
+            String input = scanner.nextLine();
+
+            if (input.equals("(")) {
+                countIn++;
+            }
+
+            else if (input.equals(")")) {
+                countOut++;
+                if (countOut - countIn != 0) {
+                    break;
+                }
+            }
+        }
+
+        if (countIn == countOut) {
+            System.out.println("BALANCED");
+        } else {
+            System.out.println("UNBALANCED");
+        }
+    }
+}
