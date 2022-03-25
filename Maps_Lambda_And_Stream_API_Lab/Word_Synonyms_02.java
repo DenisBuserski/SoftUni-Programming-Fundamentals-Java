3b34b6857755d60337017f5d0a198a8d0a83649f
Denis Buserski <81383938+DenisBuserski@users.noreply.github.com>
2022-03-25 21:20:57 +0200

    Add files via upload

---
+++ Maps_Lambda_And_Stream_API_Lab/Word_Synonyms_02.java
@@ -0,0 +1,25 @@
+package MapsLambdaAndStreamAPILab;
+
+import java.util.*;
+
+public class WordSynonyms {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int countOfPairs = Integer.parseInt(scanner.nextLine());
+
+        Map<String, List<String>> synonymDictionary = new LinkedHashMap<>();
+
+        for (int i = 0; i < countOfPairs; i++) {
+            String word = scanner.nextLine();
+            String synonym = scanner.nextLine();
+
+            synonymDictionary.putIfAbsent(word, new ArrayList<>());
+            synonymDictionary.get(word).add(synonym);
+        }
+
+        for (Map.Entry<String, List<String>> entry : synonymDictionary.entrySet()) {
+            System.out.printf("%s - %s%n", entry.getKey(), String.join(", ", entry.getValue()));
+        }
+    }
+}
