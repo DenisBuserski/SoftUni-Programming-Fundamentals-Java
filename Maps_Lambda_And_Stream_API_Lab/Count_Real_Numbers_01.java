d33c2b64cdff18971f9e37b951887ced4b0415a5
Denis Buserski <81383938+DenisBuserski@users.noreply.github.com>
2022-03-25 21:19:32 +0200

    Update Count_Real_Numbers_01.java

--- Maps_Lambda_And_Stream_API_Lab/Count_Real_Numbers_01.java
+++ Maps_Lambda_And_Stream_API_Lab/Count_Real_Numbers_01.java
@@ -1,23 +1,18 @@
-package MapsLambdaAndStreamAPILab;
-
 import java.text.DecimalFormat;
 import java.util.Arrays;
 import java.util.Map;
 import java.util.Scanner;
 import java.util.TreeMap;
 
-public class CountRealNumbers {
+public class Count_Real_Numbers_01 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
         String[] input = scanner.nextLine().split(" ");
-
         Map<Double, Integer> numbers = new TreeMap<>();
-
         double[] nums = Arrays.stream(input).mapToDouble(Double::parseDouble).toArray();
 
         for (double number : nums) {
-
             if (!numbers.containsKey(number)) {
                 numbers.put(number, 1);
             } else {
@@ -26,10 +21,12 @@ public class CountRealNumbers {
                 numbers.put(number, occurrences);
             }
         }
+        
         DecimalFormat decimalFormat = new DecimalFormat("#.#######");
 
         for (Map.Entry<Double, Integer> entry : numbers.entrySet()) {
             System.out.printf("%s -> %d%n", decimalFormat.format(entry.getKey()), entry.getValue());
         }
+        
     }
 }
