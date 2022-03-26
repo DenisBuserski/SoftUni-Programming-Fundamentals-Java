2f24f68b264d2d75d5a8dd95c97090efbbf7f0a1
Denis Buserski <81383938+DenisBuserski@users.noreply.github.com>
2022-03-26 13:55:41 +0200

    Add files via upload

---
+++ Maps_Lambda_And_Stream_API_Lab/Odd_Occurrences_03.java
@@ -0,0 +1,40 @@
+package MapsLambdaAndStreamAPILab;
+
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.Scanner;
+
+public class OddOccurrences {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String [] words = scanner.nextLine().split("\\s+");
+
+        LinkedHashMap<String, Integer> counts = new LinkedHashMap<>();
+
+        for (String word : words) {
+            String wordInLowerCase = word.toLowerCase();
+
+            if (counts.containsKey(wordInLowerCase)) {
+                counts.put(wordInLowerCase, counts.get(wordInLowerCase) + 1);
+            } else {
+                counts.put(wordInLowerCase, 1);
+            }
+        }
+
+        ArrayList<String> odds = new ArrayList<>();
+
+        for (var entry : counts.entrySet()) {
+            if (entry.getValue() % 2 == 1) {
+                odds.add((entry.getKey()));
+            }
+        }
+
+        for (int i = 0; i < odds.size(); i++) {
+            System.out.print(odds.get(i));
+            if (i < odds.size() - 1) {
+                System.out.print(", ");
+            }
+        }
+    }
+}
