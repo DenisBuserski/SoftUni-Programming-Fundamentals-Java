d5871d4c2c589a1231d4d4dd013b5af371ce62ba
Denis Buserski <81383938+DenisBuserski@users.noreply.github.com>
2022-03-26 13:59:40 +0200

    Update Largest_3_Numbers_05.java

--- Maps_Lambda_And_Stream_API_Lab/Largest_3_Numbers_05.java
+++ Maps_Lambda_And_Stream_API_Lab/Largest_3_Numbers_05.java
@@ -1,9 +1,7 @@
-package MapsLambdaAndStreamAPILab;
-
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class Largest3Numbers {
+public class Largest_3_Numbers_05 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
