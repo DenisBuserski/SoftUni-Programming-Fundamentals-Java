082d1a382f1cfa3673083246a92b6103cabfe56f
Denis Buserski <81383938+DenisBuserski@users.noreply.github.com>
2022-03-27 22:15:37 +0300

    Add files via upload

---
+++ Basic_Syntax_Conditional_Statements_And_Loops_More_Exercise/English_Name_Of_The_Last_Digit_02.java
@@ -0,0 +1,49 @@
+package BasicSyntaxConditionalStatementsAndLoopsMoreExercise;
+
+import java.util.Scanner;
+
+public class EnglishNameOfTheLastDigit {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int number = Integer.parseInt(scanner.nextLine());
+
+        int lastNum = number % 10;
+
+        switch (lastNum) {
+            case 0:
+                System.out.println("zero");
+                break;
+            case 1:
+                System.out.println("one");
+                break;
+            case 2:
+                System.out.println("two");
+                break;
+            case 3:
+                System.out.println("three");
+                break;
+            case 4:
+                System.out.println("four");
+                break;
+            case 5:
+                System.out.println("five");
+                break;
+            case 6:
+                System.out.println("six");
+                break;
+            case 7:
+                System.out.println("seven");
+                break;
+            case 8:
+                System.out.println("eight");
+                break;
+            case 9:
+                System.out.println("nine");
+                break;
+
+        }
+
+
+    }
+}
