3e03dfe0fbca784b269b66538735fba80bfd96be
Denis Buserski <81383938+DenisBuserski@users.noreply.github.com>
2022-04-03 20:07:44 +0300

    Add files via upload

---
+++ Text_Processing_More_Exercise/Ascii_Sumator_02.java
@@ -0,0 +1,29 @@
+package TextProcessingMoreExercise;
+
+import java.util.Scanner;
+
+public class AsciiSumator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        char symbol1 = scanner.nextLine().charAt(0);
+        char symbol2 = scanner.nextLine().charAt(0);
+        String text = scanner.nextLine();
+        StringBuilder newText = new StringBuilder();
+
+        for (int i = 0; i <= text.length() - 1; i++) {
+            char letter = text.charAt(i);
+
+            if (letter > symbol1 && letter < symbol2) {
+                newText.append(letter);
+            }
+        }
+
+        int result = 0;
+        for (int i = 0; i <= newText.length() - 1; i++) {
+            int number = newText.charAt(i);
+            result += number;
+        }
+        System.out.println(result);
+    }
+}
