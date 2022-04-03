16344d60350817c5a6223db0aa43e74af58a17c8
Denis Buserski <81383938+DenisBuserski@users.noreply.github.com>
2022-04-03 20:00:12 +0300

    Add files via upload

---
+++ Text_Processing_More_Exercise/Extract_Person_Information_01.java
@@ -0,0 +1,25 @@
+package TextProcessingMoreExercise;
+
+import java.util.Scanner;
+
+public class ExtractPersonInformation {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = Integer.parseInt(scanner.nextLine());
+
+        for (int i = 0; i < n; i++) {
+            String input = scanner.nextLine();
+
+            int indexOfNameStart = input.indexOf('@');
+            int indexOfNameEnd = input.indexOf('|');
+            String name = input.substring(indexOfNameStart + 1, indexOfNameEnd);
+            int indexOfAgeStart = input.indexOf('#');
+            int indexOfAgeEnd = input.indexOf('*');
+            String age = input.substring(indexOfAgeStart + 1, indexOfAgeEnd);
+
+            System.out.printf("%s is %s years old.%n", name, age);
+
+        }
+    }
+}
