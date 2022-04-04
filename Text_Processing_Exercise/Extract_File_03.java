06a45ff4d087e3ca58d52a8630a8d9c1c9730b3d
Denis Buserski <81383938+DenisBuserski@users.noreply.github.com>
2022-04-04 08:31:14 +0300

    Add files via upload

---
+++ Text_Processing_Exercise/Extract_File_03.java
@@ -0,0 +1,23 @@
+package TextProcessingExercise;
+
+import java.util.Scanner;
+
+public class ExtractFile {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String[] input = scanner.nextLine().split("\\\\");
+
+        String result = input[input.length - 1];
+        String fileName = result.split("\\.")[0];
+        String extension = result.split("\\.")[1];
+
+        System.out.print("File name: " + fileName);
+        System.out.println();
+        System.out.print("File extension: " + extension);
+
+
+
+
+    }   
+}
