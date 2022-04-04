a741ea66ad057271c5126c80537f6fa535ce6049
Denis Buserski <81383938+DenisBuserski@users.noreply.github.com>
2022-04-04 08:21:17 +0300

    Add files via upload

---
+++ Text_Processing_Exercise/Valid_Usernames_01.java
@@ -0,0 +1,34 @@
+package TextProcessingExercise;
+
+import java.util.Scanner;
+
+public class ValidUsernames {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String text = scanner.nextLine();
+        String[] usernames = text.split(", ");
+
+        for (String username : usernames) { //ако е валиден -> печатаме го
+            if(isValidUsername(username)) {
+                System.out.println(username);
+            }
+        }
+
+    }
+    static boolean isValidUsername (String username) {
+
+        if (username.length() < 3 || username.length() > 16) {
+            return false;
+        }
+
+        for (int index = 0; index <= username.length() - 1; index++) {
+            char currentSymbol = username.charAt(index);
+            if(!Character.isLetterOrDigit(currentSymbol) && currentSymbol != '-' && currentSymbol != '_') {
+                return false;
+            }
+        }
+        return true;
+
+    }
+}
