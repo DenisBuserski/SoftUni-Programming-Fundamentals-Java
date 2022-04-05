fe00b92f305a5e118580baf7a5aa44235267ed30
Denis Buserski <81383938+DenisBuserski@users.noreply.github.com>
2022-04-05 08:17:07 +0300

    Update Caesar_Cipher_04.java

--- Text_Processing_Exercise/Caesar_Cipher_04.java
+++ Text_Processing_Exercise/Caesar_Cipher_04.java
@@ -1,15 +1,11 @@
-package TextProcessingExercise;
-
 import java.util.Scanner;
 
-public class CaesarCipher {
+public class Caesar_Cipher_04 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
         String input = scanner.nextLine();
 
         StringBuilder output = new StringBuilder();
-
 
         for (int i = 0; i <= input.length() - 1; i++) {
             char letter = input.charAt(i);
@@ -19,7 +15,5 @@ public class CaesarCipher {
         }
 
         System.out.println(output.toString());
-
-
     }
 }
