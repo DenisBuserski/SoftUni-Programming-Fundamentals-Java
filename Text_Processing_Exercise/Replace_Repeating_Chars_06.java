9155d969ffd87c36c1402d9c7aed1dfee48763c8
Denis Buserski <81383938+DenisBuserski@users.noreply.github.com>
2022-04-05 08:25:36 +0300

    Update Replace_Repeating_Chars_06.java

--- Text_Processing_Exercise/Replace_Repeating_Chars_06.java
+++ Text_Processing_Exercise/Replace_Repeating_Chars_06.java
@@ -1,11 +1,9 @@
-package TextProcessingExercise;
-
 import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 import java.util.stream.Collectors;
 
-public class ReplaceRepeatingChars {
+public class Replace_Repeating_Chars_06 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
