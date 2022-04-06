30e94c12c1ab47e8d6a69f0a8434849833f91a04
Denis Buserski <81383938+DenisBuserski@users.noreply.github.com>
2022-04-06 12:13:10 +0300

    Update The_Lift_02.java

--- 01_Programming_Fundamentals_Mid_Exam_Retake/The_Lift_02.java
+++ 01_Programming_Fundamentals_Mid_Exam_Retake/The_Lift_02.java
@@ -1,12 +1,11 @@
-package ProgrammingFundamentalsMidExamRetake01;
-
 import java.util.Arrays;
 import java.util.Scanner;
 import java.util.stream.IntStream;
 
-public class TheLift {
+public class The_Lift_02 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        
         int people = Integer.parseInt(scanner.nextLine());
         int[] lift = Arrays.stream(scanner.nextLine().split("\\s+")).mapToInt(Integer::parseInt).toArray();
 
@@ -21,14 +20,15 @@ public class TheLift {
                 }
             }
         }
+        
         boolean full = IntStream.range(0, lift.length).noneMatch(i -> lift[i] != 4);
+        
         if (people == 0 && !full) {
             System.out.println("The lift has empty spots!");
         } else if (people > 0 && full) {
             System.out.printf("There isn't enough space! %d people in a queue!%n", people);
         }
+        
         System.out.print(Arrays.toString(lift).replaceAll("[\\[\\]]", "").replaceAll(", ", " "));
-
-
     }
 }
