5d49dccf3556ee2a594b865442a96e5a5f686b42
Denis Buserski <81383938+DenisBuserski@users.noreply.github.com>
2022-04-07 15:08:30 +0300

    Add files via upload

---
+++ Methods_Lab/Grades_02.java
@@ -0,0 +1,31 @@
+package MethodsLab;
+
+import java.util.Scanner;
+
+public class Grades {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        double grades = Double.parseDouble(scanner.nextLine());
+        
+        printWords(grades);
+    }
+
+    private static void printWords(double grades) {
+        if (grades >= 2.00 && grades <= 2.99) {
+            System.out.println("Fail");
+        }
+        else if (grades >= 3.00 && grades <= 3.49) {
+            System.out.println("Poor");
+        }
+        else if (grades >= 3.50 && grades <= 4.49) {
+            System.out.println("Good");
+        }
+        else if (grades >= 4.50 && grades <= 5.49) {
+            System.out.println("Very good");
+        }
+        else if (grades >= 5.50 && grades <= 6.00) {
+            System.out.println("Excellent");
+        }
+    }
+}
