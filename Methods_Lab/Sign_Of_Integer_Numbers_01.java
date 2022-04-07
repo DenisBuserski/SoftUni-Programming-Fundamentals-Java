7521522579a0334f030f961e823a1cf7a3d13659
Denis Buserski <81383938+DenisBuserski@users.noreply.github.com>
2022-04-07 15:01:04 +0300

    Add files via upload

---
+++ Methods_Lab/Sign_Of_Integer_Numbers_01.java
@@ -0,0 +1,24 @@
+package MethodsLab;
+
+import java.util.Scanner;
+
+public class SignOfIntegerNumbers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        int number = Integer.parseInt(scanner.nextLine());
+        
+        printSign(number);
+    }
+
+    private static void printSign(int number) {
+        if (number < 0) {
+            System.out.printf("The number %d is negative.", number);
+        } else if (number > 0) {
+            System.out.printf("The number %d is positive.", number);
+        } else {
+            System.out.printf("The number %d is zero.", number);
+        }
+    }
+
+}
