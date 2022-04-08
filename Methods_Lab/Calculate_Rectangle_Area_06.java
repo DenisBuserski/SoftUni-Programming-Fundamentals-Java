e1191dadc6a73585b0c879b97fb480c6afdad121
Denis Buserski <81383938+DenisBuserski@users.noreply.github.com>
2022-04-08 22:38:48 +0300

    Add files via upload

---
+++ Methods_Lab/Calculate_Rectangle_Area_06.java
@@ -0,0 +1,20 @@
+package MethodsLab;
+
+import java.util.Scanner;
+
+public class CalculateRectangleArea {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        double width = Double.parseDouble(scanner.nextLine());
+        double length = Double.parseDouble(scanner.nextLine());
+
+        double area = getRectangleArea(width, length);
+        System.out.printf("%.0f", area);
+    }
+
+    private static double getRectangleArea(double width, double length) {
+        double rectangleArea = width * length;
+        return rectangleArea;
+    }
+}
