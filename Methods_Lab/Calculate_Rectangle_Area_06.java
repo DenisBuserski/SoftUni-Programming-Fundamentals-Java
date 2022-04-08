1b00e433c32945bfb6d72e3c8337c205f2105439
Denis Buserski <81383938+DenisBuserski@users.noreply.github.com>
2022-04-08 22:39:27 +0300

    Update Calculate_Rectangle_Area_06.java

--- Methods_Lab/Calculate_Rectangle_Area_06.java
+++ Methods_Lab/Calculate_Rectangle_Area_06.java
@@ -1,8 +1,6 @@
-package MethodsLab;
-
 import java.util.Scanner;
 
-public class CalculateRectangleArea {
+public class Calculate_Rectangle_Area_06 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
