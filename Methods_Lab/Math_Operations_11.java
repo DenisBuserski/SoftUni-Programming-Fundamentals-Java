e39e8d2c52ab987a7a2e7ee636335b5e43a920d0
Denis Buserski <81383938+DenisBuserski@users.noreply.github.com>
2022-04-09 13:44:33 +0300

    Add files via upload

---
+++ Methods_Lab/Math_Operations_11.java
@@ -0,0 +1,36 @@
+package MethodsLab;
+
+        import java.util.Scanner;
+
+public class MathOperations {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        double number1 = Double.parseDouble(scanner.nextLine());
+        String command = scanner.nextLine();
+        double number2 = Double.parseDouble(scanner.nextLine());
+
+        result(number1, command, number2);
+    }
+    private static void result(double number1, String command, double number2) {
+        double result = 0;
+
+        switch (command) {
+            case "+":
+                result = number1 + number2;
+                break;
+            case "-":
+                result = number1 - number2;
+                break;
+            case "*":
+                result = number1 * number2;
+                break;
+            case "/":
+                result = number1 / number2;
+                break;
+        }
+        System.out.printf("%.0f", result);
+    }
+}
+
+
