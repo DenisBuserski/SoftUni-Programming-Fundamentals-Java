93bf820c5b523c3dcdf79ab6f07709a5aa239682
Denis Buserski <81383938+DenisBuserski@users.noreply.github.com>
2022-04-09 13:42:23 +0300

    Add files via upload

---
+++ Methods_Lab/Multiply_Evens_By_Odds_10.java
@@ -0,0 +1,43 @@
+package MethodsLab;
+
+import java.util.Scanner;
+
+public class MultiplyEvensByOdds {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int input = Integer.parseInt(scanner.nextLine());
+        int multiplyOfEvenAndOdds = getMultipleOfEvensAndOdds(Math.abs(input));
+        System.out.println(multiplyOfEvenAndOdds);
+    }
+
+    private static int getMultipleOfEvensAndOdds(int number) {
+        int evenSum = getSumOfEvenDigits(number);
+        int oddSum = getSumOfOddDigits(number);
+        return oddSum * evenSum;
+    }
+
+    private static int getSumOfEvenDigits(int number) {
+        int evenSum = 0;
+        while (number > 0) {
+            int digit = number % 10;
+            if (digit % 2 == 0) {
+                evenSum += digit;
+            }
+            number = number / 10;
+        }
+        return evenSum;
+    }
+
+    private static int getSumOfOddDigits(int number) {
+        int oddSum = 0;
+        while (number > 0) {
+            int digit = number % 10;
+            if (number % 2 == 1) {
+                oddSum += digit;
+            }
+            number = number / 10;
+        }
+        return oddSum;
+    }
+}
