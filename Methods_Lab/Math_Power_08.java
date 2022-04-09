a98a0d575f75375d73245374cf9a44637f91fdb2
Denis Buserski <81383938+DenisBuserski@users.noreply.github.com>
2022-04-09 13:36:05 +0300

    Add files via upload

---
+++ Methods_Lab/Math_Power_08.java
@@ -0,0 +1,23 @@
+package MethodsLab;
+
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
+public class MathPower {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        double number = Double.parseDouble(scanner.nextLine());
+        int power = Integer.parseInt(scanner.nextLine());
+
+        System.out.println(new DecimalFormat("0.####").format(mathPower(number, power)));
+    }
+
+    public static double mathPower(double number, int power) {
+        double result = 1;
+        result = Math.pow(number, power);
+        return result;
+    }
+
+
+}
