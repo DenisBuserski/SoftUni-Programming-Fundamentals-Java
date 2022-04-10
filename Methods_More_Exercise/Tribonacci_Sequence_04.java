ce13306360fa4e6be5ba6ee8d7332efdd59f0ef7
Denis Buserski <81383938+DenisBuserski@users.noreply.github.com>
2022-04-10 21:39:07 +0300

    Add files via upload

---
+++ Methods_More_Exercise/Tribonacci_Sequence_04.java
@@ -0,0 +1,41 @@
+package MethodsMoreExercise;
+
+import java.util.Scanner;
+
+public class TribonacciSequence {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int index = Integer.parseInt(scanner.nextLine());
+
+        getTribonacci(index);
+    }
+
+    private static void getTribonacci(int index) {
+        int[] sequenceArray = new int[index];
+
+        if (index == 1) {
+            System.out.println(1);
+        } else if (index == 2) {
+            System.out.println("1 1");
+        } else if (index == 0) {
+            System.out.println(0);
+        } else {
+
+            for (int i = 0; i <= index; i++) {
+                if (i <= 3) {
+                    sequenceArray[0] = 1;
+                    sequenceArray[1] = 1;
+                    sequenceArray[2] = 2;
+                } else {
+                    sequenceArray[i - 1] = sequenceArray[i - 2] + sequenceArray[i - 3] + sequenceArray[i - 4];
+                }
+            }
+            for (int j = 0; j <= sequenceArray.length - 1; j++) {
+                int number = sequenceArray[j];
+                System.out.print(number + " ");
+            }
+
+        }
+    }
+}
