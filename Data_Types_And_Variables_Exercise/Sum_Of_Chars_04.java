e136f2c86aa2cdd36c3d1c3135bd43bc3344a523
Denis Buserski <81383938+DenisBuserski@users.noreply.github.com>
2022-04-15 21:56:37 +0300

    Update Sum_Of_Chars_04.java

--- Data_Types_And_Variables_Exercise/Sum_Of_Chars_04.java
+++ Data_Types_And_Variables_Exercise/Sum_Of_Chars_04.java
@@ -1,20 +1,17 @@
-package DataTypesAndVariablesExercise;
-
 import java.util.Scanner;
 
-public class SumOfChars {
+public class Sum_Of_Chars_04 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
         int n = Integer.parseInt(scanner.nextLine());
-
         int sum = 0;
 
         for (int i = 1; i <= n; i++) {
             char letter = scanner.nextLine().charAt(0);
             sum += letter;
         }
+        
         System.out.printf("The sum equals: %d", sum);
-
     }
 }
