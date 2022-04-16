86163e5c3cee3a0635f3f4dad8b62e03907171d2
Denis Buserski <81383938+DenisBuserski@users.noreply.github.com>
2022-04-16 15:35:50 +0300

    Add files via upload

---
+++ Data_Types_And_Variables_Exercise/Triples_Of_Latin_Letters_06.java
@@ -0,0 +1,27 @@
+package DataTypesAndVariablesExercise;
+
+import java.util.Scanner;
+
+public class TriplesOfLatinLetters {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = Integer.parseInt(scanner.nextLine());
+
+        for (int i = 0; i < n; i++) {
+            char firstChar = (char) ('a' + i);
+
+            for (int j = 0; j < n; j++) {
+                char secondChar = (char) ('a' + j);
+
+                for (int z = 0; z < n; z++) {
+                    char thirdChar = (char) ('a' + z);
+
+                    System.out.printf("%c%c%c%n", firstChar, secondChar, thirdChar);
+                }
+            }
+
+        }
+
+    }
+}
