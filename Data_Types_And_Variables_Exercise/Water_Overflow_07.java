bf74ae97a27ad6964e84a2eecb3fc9b2ebdac5c2
Denis Buserski <81383938+DenisBuserski@users.noreply.github.com>
2022-04-16 15:39:39 +0300

    Update Water_Overflow_07.java

--- Data_Types_And_Variables_Exercise/Water_Overflow_07.java
+++ Data_Types_And_Variables_Exercise/Water_Overflow_07.java
@@ -1,8 +1,6 @@
-package DataTypesAndVariablesExercise;
-
 import java.util.Scanner;
 
-public class WaterOverflow {
+public class Water_Overflow_07 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
@@ -12,15 +10,12 @@ public class WaterOverflow {
         for (int i = 1; i <= n; i++) {
             int liters = Integer.parseInt(scanner.nextLine());
             totalLiters += liters;
-
             if (totalLiters > 255) {
                 System.out.println("Insufficient capacity!");
                 totalLiters -= liters;
             }
-
         }
 
         System.out.println(totalLiters);
-
     }
 }
