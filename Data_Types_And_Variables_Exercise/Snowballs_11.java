de37ce726a4a66a65ed41d7906da797cdb96e141
Denis Buserski <81383938+DenisBuserski@users.noreply.github.com>
2022-04-17 19:23:09 +0300

    Add files via upload

---
+++ Data_Types_And_Variables_Exercise/Snowballs_11.java
@@ -0,0 +1,37 @@
+package DataTypesAndVariablesExercise;
+
+import java.util.Scanner;
+
+public class Snowballs {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int snowballsNumber = Integer.parseInt(scanner.nextLine());
+        double maxValue = Double.MIN_VALUE;
+        int maxSnow = Integer.MIN_VALUE;
+        int maxTime = Integer.MIN_VALUE;
+        int maxQuality = Integer.MIN_VALUE;
+        double snowballValue = 0;
+        int snowballSnow = 0;
+        int snowballTime = 0;
+        int snowballQuality = 0;
+
+        for (int i = 1; i <= snowballsNumber; i++) {
+            snowballSnow = Integer.parseInt(scanner.nextLine());
+            snowballTime = Integer.parseInt(scanner.nextLine());
+            snowballQuality = Integer.parseInt(scanner.nextLine());
+
+            snowballValue = Math.pow((snowballSnow / snowballTime), snowballQuality);
+
+            if (snowballValue > maxValue) {
+                maxValue = snowballValue;
+                maxSnow = snowballSnow;
+                maxTime = snowballTime;
+                maxQuality = snowballQuality;
+            }
+        }
+
+
+        System.out.printf("%d : %d = %.0f (%d)", maxSnow, maxTime, maxValue, maxQuality);
+    }
+}
