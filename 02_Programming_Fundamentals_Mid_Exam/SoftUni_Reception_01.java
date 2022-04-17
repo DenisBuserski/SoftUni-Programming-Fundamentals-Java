06aa89aae8424c3682bc12c26aba93124e4a5697
Denis Buserski <81383938+DenisBuserski@users.noreply.github.com>
2022-04-17 19:50:01 +0300

    Add files via upload

---
+++ 02_Programming_Fundamentals_Mid_Exam/SoftUni_Reception_01.java
@@ -0,0 +1,12 @@
+package ProgrammingFundamentalsMidExam02;
+
+import java.util.Scanner;
+
+public class SoftUniReception {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+
+    }
+}
