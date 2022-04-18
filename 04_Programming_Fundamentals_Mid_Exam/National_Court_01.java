ff57355f857a94d3d6fe0f16e171f79e62071d75
Denis Buserski <81383938+DenisBuserski@users.noreply.github.com>
2022-04-18 08:46:20 +0300

    Add files via upload

---
+++ 04_Programming_Fundamentals_Mid_Exam/National_Court_01.java
@@ -0,0 +1,28 @@
+package ProgrammingFundamentalsMidExam04;
+
+import java.util.Scanner;
+
+public class NationalCourt {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int employee1 = Integer.parseInt(scanner.nextLine());
+        int employee2 = Integer.parseInt(scanner.nextLine());
+        int employee3 = Integer.parseInt(scanner.nextLine());
+        int totalPeople = Integer.parseInt(scanner.nextLine());
+
+        //every 4th hour 1 hour break -> +1 hour
+        int totalWorkForOneHour = employee1 + employee2 + employee3;
+        int time = 0;
+
+        while (totalPeople > 0) {
+            totalPeople -= totalWorkForOneHour;
+            time++;
+            if (time % 4 == 0) {
+                time++;
+            }
+        }
+
+        System.out.printf("Time needed: %dh.", time);
+    }
+}
