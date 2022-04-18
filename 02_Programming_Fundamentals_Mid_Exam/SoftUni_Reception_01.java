12aee9cedb478371d0494f714d607d558edcab0f
Denis Buserski <81383938+DenisBuserski@users.noreply.github.com>
2022-04-18 08:33:13 +0300

    Update SoftUni_Reception_01.java

--- 02_Programming_Fundamentals_Mid_Exam/SoftUni_Reception_01.java
+++ 02_Programming_Fundamentals_Mid_Exam/SoftUni_Reception_01.java
@@ -1,12 +1,26 @@
-package ProgrammingFundamentalsMidExam02;
-
 import java.util.Scanner;
 
-public class SoftUniReception {
+public class SoftUni_Reception_01 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        int employee1 = Integer.parseInt(scanner.nextLine());
+        int employee2 = Integer.parseInt(scanner.nextLine());
+        int employee3 = Integer.parseInt(scanner.nextLine());
+        int totalPeople = Integer.parseInt(scanner.nextLine());
 
+        // Every 4th hour 1 hour break -> +1 hour
+        int totalWorkForOneHour = employee1 + employee2 + employee3;
+        int time = 0;
 
+        while (totalPeople > 0) {
+            totalPeople -= totalWorkForOneHour;
+            time++;
+            if (time % 4 == 0) {
+                time++;
+            }
+        }
+
+        System.out.printf("Time needed: %dh.", time);
     }
 }
