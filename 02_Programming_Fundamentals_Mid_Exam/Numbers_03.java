e991f4b72fa02a5155f5cc16ccbb7a0839b4ab12
Denis Buserski <81383938+DenisBuserski@users.noreply.github.com>
2022-04-18 08:39:01 +0300

    Update Numbers_03.java

--- 02_Programming_Fundamentals_Mid_Exam/Numbers_03.java
+++ 02_Programming_Fundamentals_Mid_Exam/Numbers_03.java
@@ -1,21 +1,29 @@
-package ProgrammingFundamentalsMidExam02;
-
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Scanner;
 import java.util.stream.Collectors;
 
-public class Numbers {
+public class Numbers_03 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        List<Integer> input = Arrays
+            .stream(scanner.nextLine().split("\\s+"))
+            .map(Integer::parseInt)
+            .collect(Collectors.toList());
 
-        List<Integer> input = Arrays.stream(scanner.nextLine().split("\\s+")).map(Integer::parseInt).collect(Collectors.toList());
-
-        List<Integer> averageNumbers = input.stream().filter(e -> e > input.stream()
-                .mapToDouble(Integer::doubleValue).average().orElse(0.0))
-                .sorted(Comparator.reverseOrder()).limit(5).collect(Collectors.toList());
+        List<Integer> averageNumbers = input
+                .stream()
+                .filter(e -> e > input.stream()
+                        .mapToDouble(Integer::doubleValue)
+                        .average()
+                        .orElse(0.0))
+                .sorted(Comparator.reverseOrder())
+                .limit(5)
+                .collect(Collectors.toList());
+        
+        
         if (averageNumbers.isEmpty()) {
             System.out.println("No");
         } else {
