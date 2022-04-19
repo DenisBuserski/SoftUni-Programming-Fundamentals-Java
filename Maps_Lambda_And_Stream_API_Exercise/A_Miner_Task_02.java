6852ad27b3f0e857916b7eff4bca2955c9b8466b
Denis Buserski <81383938+DenisBuserski@users.noreply.github.com>
2022-04-19 09:21:03 +0300

    Add files via upload

---
+++ Maps_Lambda_And_Stream_API_Exercise/A_Miner_Task_02.java
@@ -0,0 +1,29 @@
+package MapsLambdaAndStreamAPIExercise;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class AMinerTask {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String resource = scanner.nextLine();
+
+        Map<String, Integer> resourcesQuantity = new LinkedHashMap<>();
+
+        while (!resource.equals("stop")) {
+            int quantity = Integer.parseInt(scanner.nextLine());
+
+            if (!resourcesQuantity.containsKey(resource)) {
+                resourcesQuantity.put(resource, quantity);
+            } else {
+                resourcesQuantity.put(resource, resourcesQuantity.get(resource) + quantity);
+            }
+
+            resource = scanner.nextLine();
+        }
+
+        resourcesQuantity.forEach((k, v) -> System.out.println(k + " -> " + v));
+    }
+}
