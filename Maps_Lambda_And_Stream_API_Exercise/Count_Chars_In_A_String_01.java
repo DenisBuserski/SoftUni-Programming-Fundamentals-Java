f779390c63e65b80c7f8ba498bcd4c640b8753c5
Denis Buserski <81383938+DenisBuserski@users.noreply.github.com>
2022-04-19 09:14:01 +0300

    Add files via upload

---
+++ Maps_Lambda_And_Stream_API_Exercise/Count_Chars_In_A_String_01.java
@@ -0,0 +1,34 @@
+package MapsLambdaAndStreamAPIExercise;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class CountCharsInAString {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String text = scanner.nextLine();
+
+        Map<Character, Integer> lettersCount = new LinkedHashMap<>();
+
+        for (int index = 0; index <= text.length() - 1; index++) {
+            char currentSymbol = text.charAt(index);
+
+            if (currentSymbol == ' ') {
+                continue;
+            }
+
+            if (!lettersCount.containsKey(currentSymbol)) {
+                lettersCount.put(currentSymbol, 1);
+            } else {
+                lettersCount.put(currentSymbol, lettersCount.get(currentSymbol) + 1);
+            }
+        }
+
+
+        for (Map.Entry<Character, Integer> entry : lettersCount.entrySet()) {
+            System.out.println(entry.getKey() + " -> " + entry.getValue());
+        }
+    }
+}
