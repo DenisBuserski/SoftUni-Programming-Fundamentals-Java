9a03ed1ad5586a24909c1d817c02de8e871f3dfd
Denis Buserski <81383938+DenisBuserski@users.noreply.github.com>
2022-04-20 14:43:50 +0300

    Update Orders_04.java

--- Maps_Lambda_And_Stream_API_Exercise/Orders_04.java
+++ Maps_Lambda_And_Stream_API_Exercise/Orders_04.java
@@ -1,10 +1,8 @@
-package MapsLambdaAndStreamAPIExercise;
-
 import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.Scanner;
 
-public class Orders {
+public class Orders_04 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
