d8bb0161e471aa4c24c0d4feb6e3fd486d7d3314
Denis Buserski <81383938+DenisBuserski@users.noreply.github.com>
2022-04-21 09:13:03 +0300

    Add files via upload

---
+++ Maps_Lambda_And_Stream_API_Exercise/SoftUni_Exam_Results_10.java
@@ -0,0 +1,58 @@
+package MapsLambdaAndStreamAPIExercise;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class SoftUniExamResults {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        Map<String, Integer> userPoints = new HashMap<>();
+        Map<String, Integer> languageCount = new HashMap<>();
+        String input = scanner.nextLine();
+
+        while (!input.equals("exam finished")) {
+            String[] splitArray = input.split("-");
+            String username = splitArray[0];
+
+            if (splitArray.length == 3) {
+                String language = splitArray[1];
+                int points = Integer.parseInt(splitArray[2]);
+
+                if (!userPoints.containsKey(username)) {
+                    userPoints.put(username, points);
+                } else {
+                    int currentPoints = userPoints.get(username);
+
+                    if (points > currentPoints) {
+                        userPoints.put(username, points);
+                    }
+                }
+
+                if (!languageCount.containsKey(language)) {
+                    languageCount.put(language, 1);
+                } else {
+                    languageCount.put(language, languageCount.get(language) + 1);
+                }
+
+            } else {
+                userPoints.remove(username);
+            }
+            input = scanner.nextLine();
+        }
+
+        System.out.println("Results:");
+        userPoints.entrySet().stream()
+                .sorted(Map.Entry.<String, Integer>comparingByValue().reversed()
+                        .thenComparing(Map.Entry.comparingByKey()))
+                .forEach(e -> System.out.println(e.getKey() + " | " + e.getValue()));
+
+        System.out.println("Submissions:");
+        languageCount.entrySet().stream()
+                .sorted(Map.Entry.<String, Integer>comparingByValue().reversed()
+                        .thenComparing(Map.Entry.comparingByKey()))
+                .forEach(e -> System.out.println(e.getKey() + " - " + e.getValue()));
+
+    }
+}
