586548ba7686434a1b72471feca64fc6fa69ea03
Denis Buserski <81383938+DenisBuserski@users.noreply.github.com>
2022-04-23 15:03:16 +0300

    Add files via upload

---
+++ 01_Programming_Fundamentals_Final_Exam_Retake/The_Imitation_Game_01.java
@@ -0,0 +1,45 @@
+package ProgrammingFundamentalsFinalExamRetake01;
+
+import java.util.Scanner;
+
+public class TheImitationGame {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String encryptedMessage = scanner.nextLine();
+
+        String command = scanner.nextLine();
+        while (!command.equals("Decode")) {
+            String [] commandArray = command.split("\\|");
+            String commandName = commandArray[0];
+
+            switch (commandName) {
+                case "Move":
+                    int numberOfLetters = Integer.parseInt(commandArray[1]);
+
+                    for (int i = 0; i < numberOfLetters; i++) {
+                        char symbol = encryptedMessage.charAt(i);
+                        encryptedMessage = encryptedMessage.concat(String.valueOf(symbol));
+                    }
+                    StringBuilder encryptedMessage2 = new StringBuilder(encryptedMessage);
+                    encryptedMessage = encryptedMessage2.delete(0, numberOfLetters).toString();
+                    break;
+                case "Insert":
+                    int index = Integer.parseInt(commandArray[1]);
+                    String value = commandArray[2];
+
+                    StringBuilder encryptedMessage1 = new StringBuilder(encryptedMessage);
+                    encryptedMessage1.insert(index, value);
+                    encryptedMessage = encryptedMessage1.toString();
+                    break;
+                case "ChangeAll":
+                    String substring = commandArray[1];
+                    String replacement = commandArray[2];
+                    encryptedMessage = encryptedMessage.replace(substring, replacement);
+                    break;
+            }
+            command = scanner.nextLine();
+        }
+        System.out.printf("The decrypted message is: %s", encryptedMessage);
+    }
+}
---
+++ 01_Programming_Fundamentals_Final_Exam_Retake/The_Pianist_03.java
@@ -0,0 +1,69 @@
+package ProgrammingFundamentalsFinalExamRetake01;
+
+import java.util.*;
+
+public class ThePianist {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int numberOfPieces = Integer.parseInt(scanner.nextLine());
+
+        Map<String, List<String>> pieces = new HashMap<>();
+
+        for (int i = 1; i <= numberOfPieces; i++) {
+            String pieceInfo = scanner.nextLine();
+            String [] pieceInfoArray = pieceInfo.split("\\|");
+            String piece = pieceInfoArray[0];
+            String composer = pieceInfoArray[1];
+            String key = pieceInfoArray[2];
+
+            pieces.put(piece, Arrays.asList(composer, key));
+        }
+
+        String command = scanner.nextLine();
+        while (!command.equals("Stop")) {
+            String [] commandArray = command.split("\\|");
+            String commandName = commandArray[0];
+            String piece = commandArray[1];
+
+            switch (commandName) {
+                case "Add":
+                    String composer = commandArray[2];
+                    String key = commandArray[3];
+
+                    if (pieces.containsKey(piece)) {
+                        System.out.printf("%s is already in the collection!%n", piece);
+                    } else {
+                        pieces.put(piece, Arrays.asList(composer, key));
+                        System.out.printf("%s by %s in %s added to the collection!%n", piece, composer, key);
+                    }
+                    break;
+                case "Remove":
+
+                    if (pieces.containsKey(piece)) {
+                        pieces.remove(piece);
+                        System.out.printf("Successfully removed %s!%n", piece);
+                    } else {
+                        System.out.printf("Invalid operation! %s does not exist in the collection.%n", piece);
+                    }
+                    break;
+                case "ChangeKey":
+                    String newKey = commandArray[2];
+
+                    if (pieces.containsKey(piece)) {
+                        pieces.get(piece).set(1, newKey);
+                        System.out.printf("Changed the key of %s to %s!%n", piece, newKey);
+                    } else {
+                        System.out.printf("Invalid operation! %s does not exist in the collection.%n", piece);
+                    }
+                    break;
+            }
+            command = scanner.nextLine();
+        }
+
+        pieces.entrySet().stream()
+                .sorted(Map.Entry.comparingByKey())
+                .forEach(kvp -> System.out.printf("%s -> Composer: %s, Key: %s%n",
+                        kvp.getKey(), kvp.getValue().get(0), kvp.getValue().get(1)));
+    }
+}
