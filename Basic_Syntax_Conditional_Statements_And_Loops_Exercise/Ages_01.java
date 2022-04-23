637c86cdc0ef78de70ba8b12ca7444d48f2db96f
Denis Buserski <81383938+DenisBuserski@users.noreply.github.com>
2022-04-23 22:59:23 +0300

    Update Ages_01.java

--- Basic_Syntax_Conditional_Statements_And_Loops_Exercise/Ages_01.java
+++ Basic_Syntax_Conditional_Statements_And_Loops_Exercise/Ages_01.java
@@ -1,8 +1,6 @@
-package BasicSyntaxConditionalStatementsAndLoopsExercise;
-
 import java.util.Scanner;
 
-public class Ages {
+public class Ages_01 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
