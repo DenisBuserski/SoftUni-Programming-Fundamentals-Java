2d7d44e5b5b9fbb01c9e30b65137ff05bc75e180
Denis Buserski <81383938+DenisBuserski@users.noreply.github.com>
2022-04-23 22:58:17 +0300

    Add files via upload

---
+++ Basic_Syntax_Conditional_Statements_And_Loops_Exercise/Ages_01.java
@@ -0,0 +1,26 @@
+package BasicSyntaxConditionalStatementsAndLoopsExercise;
+
+import java.util.Scanner;
+
+public class Ages {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int age = Integer.parseInt(scanner.nextLine());
+        String output = "";
+
+        if (age >= 0 && age <= 2) {
+            output = "baby";
+        } else if (age >= 3 && age <= 13) {
+            output = "child";
+        } else if (age >= 14 && age <= 19) {
+            output = "teenager";
+        } else if (age >= 20 && age <= 65) {
+            output = "adult";
+        } else if (age >= 66) {
+            output = "elder";
+        }
+
+        System.out.printf("%s", output);
+    }
+}
