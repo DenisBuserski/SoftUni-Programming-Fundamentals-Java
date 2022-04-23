de4ad2ef0503c8eafbe865c65ea2e7920932d8de
Denis Buserski <81383938+DenisBuserski@users.noreply.github.com>
2022-04-23 23:01:48 +0300

    Update Division_02.java

--- Basic_Syntax_Conditional_Statements_And_Loops_Exercise/Division_02.java
+++ Basic_Syntax_Conditional_Statements_And_Loops_Exercise/Division_02.java
@@ -1,17 +1,15 @@
-package BasicSyntaxConditionalStatementsAndLoopsExercise;
-
 import java.util.Scanner;
 
-public class Division {
+public class Division_02 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
         int number = Integer.parseInt(scanner.nextLine());
 
         // 2, 3, 6, 7, 10
-        // най-големият делител
-        // ако се дели на 2 и 3 значи се дели на 6 и се пише 6
-        // ако се дели на 2 понякога се дели и на 10 и се пише 10
+        // Най-големият делител
+        // Ако се дели на 2 и 3 значи се дели на 6 и се пише 6
+        // Ако се дели на 2 понякога се дели и на 10 и се пише 10
 
         if (number % 10 == 0) {
             System.out.println("The number is divisible by 10");
