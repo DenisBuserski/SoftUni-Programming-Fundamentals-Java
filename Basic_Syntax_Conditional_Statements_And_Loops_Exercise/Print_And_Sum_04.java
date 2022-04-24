0cd9be98ccaae91178262dd8b4311ae86754dc25
Denis Buserski <81383938+DenisBuserski@users.noreply.github.com>
2022-04-24 13:09:14 +0300

    Add files via upload

---
+++ Basic_Syntax_Conditional_Statements_And_Loops_Exercise/Print_And_Sum_04.java
@@ -0,0 +1,21 @@
+package BasicSyntaxConditionalStatementsAndLoopsExercise;
+
+import java.util.Scanner;
+
+public class PrintAndSum {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int startNumber = Integer.parseInt(scanner.nextLine());
+        int endNumber = Integer.parseInt(scanner.nextLine());
+
+        int sum = 0;
+        for (int i = startNumber; i <= endNumber; i++) {
+            sum += i;
+            System.out.printf("%d ", i);
+        }
+        System.out.println("");
+        System.out.printf("Sum: %d", sum);
+
+    }
+}
