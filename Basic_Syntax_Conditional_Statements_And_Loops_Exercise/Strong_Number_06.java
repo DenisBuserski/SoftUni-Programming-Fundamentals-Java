b85647e2ebe4fc475fb4439b1867216d8f7bb560
Denis Buserski <81383938+DenisBuserski@users.noreply.github.com>
2022-04-24 13:13:35 +0300

    Update Strong_Number_06.java

--- Basic_Syntax_Conditional_Statements_And_Loops_Exercise/Strong_Number_06.java
+++ Basic_Syntax_Conditional_Statements_And_Loops_Exercise/Strong_Number_06.java
@@ -1,26 +1,22 @@
-package BasicSyntaxConditionalStatementsAndLoopsExercise;
-
 import java.util.Scanner;
 
-public class StrongNumber {
+public class Strong_Number_06 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
         int number = Integer.parseInt(scanner.nextLine());
         int startNumber = number;
-
         int sumFact = 0;
 
         while (number != 0) {
             int digit = number % 10;
-
             int fact = 1;
 
             for (int i = 1; i <= digit; i++) {
                 fact = fact * i;
             }
             sumFact += fact;
-            //4. премахваме последната цифра от числото
+            // Премахваме последната цифра от числото
             number /= 10;
         }
 
