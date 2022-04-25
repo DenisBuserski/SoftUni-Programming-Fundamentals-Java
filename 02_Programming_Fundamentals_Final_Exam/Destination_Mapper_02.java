3d7b632827ad3b413aea8227faa9f4aec2b686bb
Denis Buserski <81383938+DenisBuserski@users.noreply.github.com>
2022-04-25 13:55:55 +0300

    Update Destination_Mapper_02.java

--- 02_Programming_Fundamentals_Final_Exam/Destination_Mapper_02.java
+++ 02_Programming_Fundamentals_Final_Exam/Destination_Mapper_02.java
@@ -1,12 +1,10 @@
-package ProgrammingFundamentalsFinalExam02;
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class DestinationMapper {
+public class Destination_Mapper_02 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
