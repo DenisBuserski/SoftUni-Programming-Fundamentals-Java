86b941726f361deeba9c3a7c2870d930e278337b
Denis Buserski <81383938+DenisBuserski@users.noreply.github.com>
2022-04-25 13:45:37 +0300

    Update Triangle_Of_Numbers_08.java

--- Basic_Syntax_Conditional_Statements_And_Loops_Exercise/Triangle_Of_Numbers_08.java
+++ Basic_Syntax_Conditional_Statements_And_Loops_Exercise/Triangle_Of_Numbers_08.java
@@ -1,20 +1,17 @@
-package BasicSyntaxConditionalStatementsAndLoopsExercise;
-
 import java.util.Scanner;
 
-public class TriangleOfNumbers {
+public class Triangle_Of_Numbers_08 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
         int n = Integer.parseInt(scanner.nextLine());
 
         for (int row = 1; row <= n ; row++) {
-
             for (int count = 1; count <= row ; count++) {
                 System.out.print(row + " ");
             }
             System.out.println();
-
         }
+        
     }
 }
