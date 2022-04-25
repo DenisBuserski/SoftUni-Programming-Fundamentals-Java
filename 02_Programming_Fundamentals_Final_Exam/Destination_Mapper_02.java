a8f23b0c8146805334ee929fa9243959b68dff4b
Denis Buserski <81383938+DenisBuserski@users.noreply.github.com>
2022-04-25 13:55:11 +0300

    Add files via upload

---
+++ 02_Programming_Fundamentals_Final_Exam/Destination_Mapper_02.java
@@ -0,0 +1,31 @@
+package ProgrammingFundamentalsFinalExam02;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class DestinationMapper {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String destinations = scanner.nextLine();
+        List<String> destinationsList = new ArrayList<>();
+
+        String regex = "(?<one>[\\/|=])(?<match>[A-Z]{1}[A-z]{2,})\\1";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(destinations);
+        int travelPoints = 0;
+
+        while (matcher.find()) {
+            destinationsList.add(matcher.group("match"));
+            travelPoints += matcher.group("match").length();
+        }
+
+        System.out.print("Destinations: ");
+        System.out.print(destinationsList.toString().replaceAll("[\\[\\]]", ""));
+        System.out.println();
+        System.out.printf("Travel Points: %d", travelPoints);
+    }
+}
---
+++ 02_Programming_Fundamentals_Final_Exam/World_Tour_01.java
@@ -0,0 +1,54 @@
+package ProgrammingFundamentalsFinalExam02;
+
+import java.util.Scanner;
+
+public class WorldTour {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String stops = scanner.nextLine();
+        StringBuilder stopsNew = new StringBuilder(stops);
+
+        String command = scanner.nextLine();
+        while (!command.equals("Travel")) {
+            String [] commandArray = command.split("\\:");
+
+            switch (commandArray[0]) {
+                case "Add Stop":
+                    int addIndex = Integer.parseInt(commandArray[1]);
+                    String addStop = commandArray[2];
+
+                    if (addIndex >= 0 && addIndex < stopsNew.length()) {
+                        stopsNew.insert(addIndex, addStop);
+                    }
+                    System.out.printf("%s%n", stopsNew);
+                    break;
+                case "Remove Stop":
+                    int startIndex = Integer.parseInt(commandArray[1]);
+                    int endIndex = Integer.parseInt(commandArray[2]);
+
+                    if (startIndex >= 0 && endIndex < stopsNew.length()) {
+                        stopsNew.delete(startIndex, endIndex + 1);
+                    }
+                    System.out.printf("%s%n", stopsNew);
+                    break;
+                case "Switch":
+                    String oldString = commandArray[1];
+                    String newString = commandArray[2];
+
+                    if (stopsNew.toString().contains(oldString)) {
+                        String switched = stopsNew.toString().replace(oldString, newString);
+                        stopsNew.replace(0, stopsNew.length(), switched);
+                    }
+                    System.out.printf("%s%n", stopsNew);
+                    break;
+            }
+
+            command = scanner.nextLine();
+        }
+
+        System.out.printf("Ready for world tour! Planned stops: %s%n", stopsNew.toString());
+
+
+    }
+}
