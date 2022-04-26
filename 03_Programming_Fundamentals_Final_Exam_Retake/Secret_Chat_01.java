4501338a6e8d847739a67b262c21f1f7f6c0f72b
Denis Buserski <81383938+DenisBuserski@users.noreply.github.com>
2022-04-26 07:55:34 +0300

    Update Secret_Chat_01.java

--- 03_Programming_Fundamentals_Final_Exam_Retake/Secret_Chat_01.java
+++ 03_Programming_Fundamentals_Final_Exam_Retake/Secret_Chat_01.java
@@ -1,8 +1,6 @@
-package ProgrammingFundamentalsFinalExamRetake03;
-
 import java.util.Scanner;
 
-public class SecretChat {
+public class Secret_Chat_01 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
@@ -20,26 +18,19 @@ public class SecretChat {
                     input = space.insert(index, " ").toString();
                     System.out.printf("%s%n", input);
                     break;
-
                 case "Reverse":
                     String substringToReverse = commandArray[1];
                     StringBuilder substring1 = new StringBuilder(substringToReverse);
                     String reversedSubstring = substring1.reverse().toString();
 
-
                     if (input.contains(substringToReverse)) {
                         input = input.replaceFirst(substringToReverse, "");
                         input = input.concat(reversedSubstring);
                         System.out.printf("%s%n", input);
-
                     } else {
                         System.out.printf("error%n");
-
                     }
-
-
                     break;
-
                 case "ChangeAll":
                     String substring = commandArray[1];
                     String replacement = commandArray[2];
@@ -50,7 +41,7 @@ public class SecretChat {
 
             command = scanner.nextLine();
         }
+        
         System.out.printf("You have a new text message: %s%n", input);
-
     }
 }
