e9cb579019ca91ca7612da2254b8581dea4a35d8
Denis Buserski <81383938+DenisBuserski@users.noreply.github.com>
2022-04-28 08:34:02 +0300

    Add files via upload

---
+++ Basic_Syntax_Conditional_Statements_And_Loops_Lab/Passed_02.java
@@ -0,0 +1,16 @@
+package BasicSyntaxConditionalStatementsAndLoopsLab;
+
+import java.util.Scanner;
+
+public class Passed {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        double grade = Double.parseDouble(scanner.nextLine());
+
+        if (grade >= 3.00) {
+            System.out.println("Passed!");
+        }
+
+    }
+}
