5cc01e1ea3a2adbcfe47ac5f4f7a27571684e597
Denis Buserski <81383938+DenisBuserski@users.noreply.github.com>
2022-04-29 10:22:14 +0300

    Update Divisible_By_3_08.java

--- Basic_Syntax_Conditional_Statements_And_Loops_Lab/Divisible_By_3_08.java
+++ Basic_Syntax_Conditional_Statements_And_Loops_Lab/Divisible_By_3_08.java
@@ -1,13 +1,12 @@
-package BasicSyntaxConditionalStatementsAndLoopsLab;
-
 import java.util.Scanner;
 
-public class DivisibleBy3 {
+public class Divisible_By_3_08 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
         for (int i = 3; i <= 100; i += 3) {
             System.out.println(i);
         }
+        
     }
 }
