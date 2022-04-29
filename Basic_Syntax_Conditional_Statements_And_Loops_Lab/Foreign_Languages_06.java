421238a2ccae2875385cbd818e8e30c4233cfc29
Denis Buserski <81383938+DenisBuserski@users.noreply.github.com>
2022-04-29 10:13:10 +0300

    Add files via upload

---
+++ Basic_Syntax_Conditional_Statements_And_Loops_Lab/Foreign_Languages_06.java
@@ -0,0 +1,25 @@
+package BasicSyntaxConditionalStatementsAndLoopsLab;
+
+import java.util.Scanner;
+
+public class ForeignLanguages {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String country = scanner.nextLine();
+
+        switch (country) {
+            case "England":
+            case "USA":
+                System.out.println("English");
+                break;
+            case "Spain":
+            case "Argentina":
+            case "Mexico":
+                System.out.println("Spanish");
+                break;
+            default:
+                System.out.println("unknown");
+        }
+    }
+}
