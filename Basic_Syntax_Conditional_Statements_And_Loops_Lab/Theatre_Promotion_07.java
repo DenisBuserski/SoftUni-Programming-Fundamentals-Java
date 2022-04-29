fb0d0dbd5cfb622330ff9951391cc7ac91377c10
Denis Buserski <81383938+DenisBuserski@users.noreply.github.com>
2022-04-29 10:17:13 +0300

    Update Theatre_Promotion_07.java

--- Basic_Syntax_Conditional_Statements_And_Loops_Lab/Theatre_Promotion_07.java
+++ Basic_Syntax_Conditional_Statements_And_Loops_Lab/Theatre_Promotion_07.java
@@ -1,42 +1,34 @@
-package BasicSyntaxConditionalStatementsAndLoopsLab;
-
 import java.util.Scanner;
 
-public class TheatrePromotion {
+public class Theatre_Promotion_07 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
         String typeOfDay = scanner.nextLine();
         int age = Integer.parseInt(scanner.nextLine());
-
         int price = -1;
-
 
         switch (typeOfDay) {
             case "Weekday":
                 if ((age >= 0 && age <= 18) || (age > 64 && age <= 122)) {
                     price = 12;
-                }
-                else if (age > 18 && age <= 64) {
+                } else if (age > 18 && age <= 64) {
                     price = 18;
                 }
                 break;
             case "Weekend":
                 if ((age >= 0 && age <= 18) || (age > 64 && age <= 122)) {
                     price = 15;
-                }
-                else if (age > 18 && age <= 64) {
+                } else if (age > 18 && age <= 64) {
                     price = 20;
                 }
                 break;
             case "Holiday":
                 if (age >= 0 && age <= 18) {
                     price = 5;
-                }
-                else if (age > 18 && age <= 64) {
+                } else if (age > 18 && age <= 64) {
                     price = 12;
-                }
-                else if (age > 64 && age <= 122) {
+                } else if (age > 64 && age <= 122) {
                     price = 10;
                 }
                 break;
@@ -44,9 +36,9 @@ public class TheatrePromotion {
 
         if (price == -1) {
             System.out.println("Error!");
-        }
-        else {
+        } else {
             System.out.println(price + "$");
         }
+        
     }
 }
