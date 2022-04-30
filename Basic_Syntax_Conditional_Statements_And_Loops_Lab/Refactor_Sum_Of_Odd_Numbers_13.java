5d4293fcaac820126a7925ee22c509a2f4ebf3e0
Denis Buserski <81383938+DenisBuserski@users.noreply.github.com>
2022-04-30 11:00:29 +0300

    Update Refactor_Sum_Of_Odd_Numbers_13.java

--- Basic_Syntax_Conditional_Statements_And_Loops_Lab/Refactor_Sum_Of_Odd_Numbers_13.java
+++ Basic_Syntax_Conditional_Statements_And_Loops_Lab/Refactor_Sum_Of_Odd_Numbers_13.java
@@ -1,25 +1,18 @@
-package BasicSyntaxConditionalStatementsAndLoopsLab;
-
 import java.util.Scanner;
 
-public class RefactorSumOfOddNumbers {
+public class Refactor_Sum_Of_Odd_Numbers_13 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         int n = Integer.parseInt(sc.nextLine());
-
         int sum = 0;
+        
         for (int i = 1; i <= n; i++) {
             int oddNumber = 2 * i - 1;
             sum += oddNumber;
             System.out.printf("%d%n", oddNumber);
-
         }
+        
         System.out.printf("Sum: %d", sum);
-
-
-
-
-
     }
 }
