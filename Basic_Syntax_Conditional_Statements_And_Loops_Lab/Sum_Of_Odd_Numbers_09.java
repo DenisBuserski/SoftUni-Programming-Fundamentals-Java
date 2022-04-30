16010e7ae3b356475547127ef3d7ee1498fc8de4
Denis Buserski <81383938+DenisBuserski@users.noreply.github.com>
2022-04-30 10:48:27 +0300

    Update Sum_Of_Odd_Numbers_09.java

--- Basic_Syntax_Conditional_Statements_And_Loops_Lab/Sum_Of_Odd_Numbers_09.java
+++ Basic_Syntax_Conditional_Statements_And_Loops_Lab/Sum_Of_Odd_Numbers_09.java
@@ -1,22 +1,19 @@
-package BasicSyntaxConditionalStatementsAndLoopsLab;
-
 import java.util.Scanner;
 
-public class SumOfOddNumbers {
+public class Sum_Of_Odd_Numbers_09 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
         int number = Integer.parseInt(scanner.nextLine());
-
         int sum = 0;
+        
         for (int i = 1; i <= number; i++) {
             int oddNumber = 2 * i - 1;
             sum += oddNumber;
 
             System.out.println(oddNumber);
-
-
         }
+        
         System.out.printf("Sum: %d", sum);
     }
 }
