2771f3bd47cb9f9ce5ec2ab0ebbc26f3ec67413c
Denis Buserski <81383938+DenisBuserski@users.noreply.github.com>
2022-04-30 10:49:12 +0300

    Add files via upload

---
+++ Basic_Syntax_Conditional_Statements_And_Loops_Lab/Multiplication_Table_10.java
@@ -0,0 +1,15 @@
+package BasicSyntaxConditionalStatementsAndLoopsLab;
+
+import java.util.Scanner;
+
+public class MultiplicationTable {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int number = Integer.parseInt(scanner.nextLine());
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.printf("%d X %d = %d%n", number, i, number * i);
+        }
+    }
+}
