334311f0e26dfa70077de37a62d39ea497be65e5
Denis Buserski <81383938+DenisBuserski@users.noreply.github.com>
2022-05-01 12:40:53 +0300

    Add files via upload

---
+++ Objects_And_Classes_Lab/Sum_Big_Numbers_02.java
@@ -0,0 +1,20 @@
+package ObjectsAndClassesLab;
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class SumBigNumbers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        BigInteger firstNumber = new BigInteger(scanner.nextLine());
+        BigInteger secondNumber = new BigInteger(scanner.nextLine());
+
+        BigInteger result = firstNumber.add(secondNumber);
+        System.out.println(result);
+
+
+
+
+    }
+}
