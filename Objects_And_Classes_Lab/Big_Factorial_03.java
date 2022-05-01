8528be0e54179b071a9415c42e0106ac1f7d1399
Denis Buserski <81383938+DenisBuserski@users.noreply.github.com>
2022-05-01 12:42:22 +0300

    Add files via upload

---
+++ Objects_And_Classes_Lab/Big_Factorial_03.java
@@ -0,0 +1,21 @@
+package ObjectsAndClassesLab;
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class BigFactorial {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = Integer.parseInt(scanner.nextLine());
+
+        BigInteger factorial = new BigInteger(String.valueOf(1));
+
+        for (int i = 1; i <= n; i++) {
+            factorial = factorial.multiply(BigInteger.valueOf(i));
+        }
+        System.out.println(factorial);
+
+
+    }
+}
