aad0542a3451cf1baecd619a3b6f9ff5a0c2f1e6
Denis Buserski <81383938+DenisBuserski@users.noreply.github.com>
2022-05-02 14:02:18 +0300

    Add files via upload

---
+++ Objects_And_Classes_Lab/Songs_04/Song.java
@@ -0,0 +1,49 @@
+package ObjectsAndClassesLab.Songs;
+
+public class Song {
+    private String typeList;
+    private String name;
+    private String time;
+
+
+    public Song(String typeList, String name, String time) {
+        this.typeList = typeList;
+        this.name = name;
+        this.time = time;
+    }
+
+    public String getTypeList(String typeList) {
+        return typeList;
+    }
+
+    public void setTypeList(String typeList) {
+        this.typeList = typeList;
+    }
+
+
+    public String getName(String name) {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTime(String time) {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+
+    public Object getTypeList() {
+        return typeList;
+    }
+}
+
