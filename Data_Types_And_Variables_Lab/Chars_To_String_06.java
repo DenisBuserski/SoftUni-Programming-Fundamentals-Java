62305bdee59f9cfbfe066eb83f3c103290e7dd39
Denis Buserski <81383938+DenisBuserski@users.noreply.github.com>
2022-05-04 08:12:22 +0300

    Update Chars_To_String_06.java

--- Data_Types_And_Variables_Lab/Chars_To_String_06.java
+++ Data_Types_And_Variables_Lab/Chars_To_String_06.java
@@ -1,8 +1,6 @@
-package DataTypesAndVariablesLab;
-
 import java.util.Scanner;
 
-public class CharsToString {
+public class Chars_To_String_06 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
