204f8219380f337decc1189ac1456cd0e2317c59
Denis Buserski <81383938+DenisBuserski@users.noreply.github.com>
2022-05-04 08:14:57 +0300

    Add files via upload

---
+++ Data_Types_And_Variables_Lab/Lower_Or_Upper_08.java
@@ -0,0 +1,19 @@
+package DataTypesAndVariablesLab;
+
+import java.util.Scanner;
+
+public class LowerOrUpper {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        char firstChar = scanner.nextLine().charAt(0);
+
+        if (Character.isLowerCase(firstChar)) {
+            System.out.println("lower-case");
+        }
+        else if (Character.isUpperCase(firstChar)) {
+            System.out.println("upper-case");
+        }
+
+    }
+}
