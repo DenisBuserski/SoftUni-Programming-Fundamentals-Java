e18fdca8b50e77acb120d1553c1181c918f27190
Denis Buserski <81383938+DenisBuserski@users.noreply.github.com>
2022-05-05 11:18:30 +0300

    Add files via upload

---
+++ Data_Types_And_Variables_Lab/Refactor_Volume_Of_Pyramid_11.java
@@ -0,0 +1,23 @@
+package DataTypesAndVariablesLab;
+
+import java.util.Scanner;
+
+public class RefactorVolumeOfPyramid {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        double length, width, height = 0;
+
+        System.out.print("Length: ");
+        System.out.print("Width: ");
+        System.out.print("Height: ");
+        length = Double.parseDouble(scanner.nextLine());
+        width = Double.parseDouble(scanner.nextLine());
+        height = Double.parseDouble(scanner.nextLine());
+
+        double result = (length * width * height) / 3;
+        System.out.printf("Pyramid Volume: %.2f", result);
+
+
+    }
+}
