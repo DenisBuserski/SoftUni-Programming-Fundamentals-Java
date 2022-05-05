ce9dbb7bb704653f102bcbdb4e3f9736b7995096
Denis Buserski <81383938+DenisBuserski@users.noreply.github.com>
2022-05-05 11:15:00 +0300

    Add files via upload

---
+++ Data_Types_And_Variables_Lab/Centuries_To_Minutes_09.java
@@ -0,0 +1,21 @@
+package DataTypesAndVariablesLab;
+
+import java.util.Scanner;
+
+public class CenturiesToMinutes {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int centuries = Integer.parseInt(scanner.nextLine());
+
+        int years = centuries * 100;
+
+        int days = (int) (years * 365.2422);
+
+        int hours = days * 24;
+
+        int minutes = hours * 60;
+
+        System.out.printf("%d centuries = %d years = %d days = %d hours = %d minutes", centuries, years, days, hours, minutes);
+    }
+}
