9527ac5616d16312372a8009055275a7eb2adf64
Denis Buserski <81383938+DenisBuserski@users.noreply.github.com>
2022-05-09 11:57:48 +0300

    Update Teamwork_Projects_04.java

--- Objects_And_Classes_More_Exercise/Teamwork_Projects_04.java
+++ Objects_And_Classes_More_Exercise/Teamwork_Projects_04.java
@@ -1,18 +1,16 @@
-package ObjectsAndClassesMoreExercise;
-
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Scanner;
 import java.util.stream.IntStream;
 
-public class Teamwork {
+public class Teamwork_Projects_04 {
     public static void main(String[] args) {
-
-
         Scanner scan = new Scanner(System.in);
+        
         int countOfTeams = Integer.parseInt(scan.nextLine());
         List<Team> teamsList = new ArrayList<>();
+        
         while (countOfTeams--> 0){
             String[] input = scan.nextLine().split("-".trim());
             String creator = input[0].trim();
@@ -35,46 +33,43 @@ public class Teamwork {
             String[] command = input.split("->".trim());
             String user = command[0].trim();
             String teamName = command[1].trim();
+            
             if (teamsList.stream().noneMatch((e -> e.getName().equals(teamName)))) {
                 System.out.printf("Team %s does not exist!%n", teamName);
-            } else if ((teamsList.stream().anyMatch(e -> e.getCreator().equals(user)))
-                    || (teamsList.stream().flatMap(team -> team.getMembers().stream()).anyMatch(member -> member.equals(user)))) {
+            } else if ((teamsList
+                        .stream()
+                        .anyMatch(e -> e.getCreator().equals(user))) || 
+                       (teamsList
+                        .stream()
+                        .flatMap(team -> team.getMembers().stream())
+                        .anyMatch(member -> member.equals(user)))) {
                 System.out.printf("Member %s cannot join team %s!%n", user, teamName);
             } else {
-                IntStream.range(0, teamsList.size()).filter(i -> teamsList.get(i).getName().equals(teamName))
-                        .forEach(i -> teamsList.get(i).getMembers().add(user));
+                IntStream.range(0, teamsList.size())
+                    .filter(i -> teamsList.get(i).getName().equals(teamName))
+                    .forEach(i -> teamsList.get(i).getMembers().add(user));
             }
         }
+        
         teamsList.sort(Comparator.comparing(Team::getName));
-        teamsList.sort(Comparator.comparing(Team::numberOfMembers).
-
-                reversed());
-        IntStream.range(0, teamsList.size()).
-
-                filter(i -> !teamsList.get(i).
-
-                        getMembers().
-
-                        isEmpty()).
-
-                forEach(i ->
-
-                {
+        teamsList.sort(Comparator.comparing(Team::numberOfMembers).reversed());
+        IntStream.range(0, teamsList.size())
+            .filter(i -> !teamsList.get(i).getMembers().isEmpty())
+            .forEach(i -> {
                     System.out.printf("%s%n", teamsList.get(i).getName());
                     System.out.printf("- %s%n", teamsList.get(i).getCreator());
                     teamsList.forEach(member -> member.getMembers().sort(String::compareTo));
                     teamsList.get(i).getMembers().forEach(current -> System.out.printf("-- %s%n", current));
                 });
+        
         System.out.println("Teams to disband:");
-        teamsList.stream().
-
-                filter(current -> current.getMembers().
-
-                        isEmpty()).
-
-                forEach(currentTeam -> System.out.printf("%s%n", currentTeam.getName()));
+        teamsList
+            .stream()
+            .filter(current -> current.getMembers().isEmpty())
+            .forEach(currentTeam -> System.out.printf("%s%n", currentTeam.getName()));
     }
 
+    
     static class Team {
         String name;
         String creator;
@@ -112,4 +107,5 @@ public class Teamwork {
             return members.size();
         }
     }
+    
 }
