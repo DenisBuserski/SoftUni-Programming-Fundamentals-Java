e3bea4822ecd880e2aa0ea991ebbd911ffe6f1ad
Denis Buserski <81383938+DenisBuserski@users.noreply.github.com>
2022-05-10 08:00:23 +0300

    Add files via upload

---
+++ Methods_Exercise/Characters_In_Range_03.java
@@ -0,0 +1,30 @@
+package MethodsExercise;
+
+import java.util.Scanner;
+
+public class CharactersInRange {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        char symbol1 = scanner.nextLine().charAt(0);
+        char symbol2 = scanner.nextLine().charAt(0);
+
+        printResult(symbol1, symbol2);
+
+    }
+    private static void printResult(char symbol1, char symbol2) {
+
+        if (symbol2 < symbol1) {
+            for (int i = symbol2 + 1; i < symbol1; i++) {
+                System.out.print((char) i + " ");
+            }
+        } else {
+            for (int i = symbol1 + 1; i < symbol2; i++) {
+                System.out.print((char) i + " ");
+            }
+        }
+
+
+
+    }
+}
