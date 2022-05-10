0e0e2117aa8d234a7dcf6a63746fd08173408c01
Denis Buserski <81383938+DenisBuserski@users.noreply.github.com>
2022-05-10 08:11:20 +0300

    Add files via upload

---
+++ Methods_Exercise/Middle_Characters_06.java
@@ -0,0 +1,31 @@
+package MethodsExercise;
+
+import java.util.Scanner;
+
+public class MiddleCharacters {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String word = scanner.nextLine();
+
+        oddWord(word); // нечетен
+        evenWord(word); // четен
+    }
+
+    private static void oddWord(String word) {
+        if (word.length() % 2 != 0) {
+            char middleLetter = word.charAt(word.length() / 2);
+            String letter = String.valueOf(middleLetter);
+            System.out.println(letter);
+        }
+    }
+    private static void evenWord(String word) {
+        if (word.length() % 2 == 0) {
+            char middleLetter1 = word.charAt(word.length() / 2);
+            char middleLetter2 = word.charAt((word.length() / 2) - 1);
+            String letter1 = String.valueOf(middleLetter1);
+            String letter2 = String.valueOf(middleLetter2);
+            System.out.println(letter2 + letter1);
+        }
+    }
+}
