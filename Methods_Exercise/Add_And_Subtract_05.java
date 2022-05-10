04222954d9d73e31e4321778020e47b97b80f3e2
Denis Buserski <81383938+DenisBuserski@users.noreply.github.com>
2022-05-10 08:09:55 +0300

    Add files via upload

---
+++ Methods_Exercise/Add_And_Subtract_05.java
@@ -0,0 +1,25 @@
+package MethodsExercise;
+
+import java.util.Scanner;
+
+public class AddAndSubtract {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int number1 = Integer.parseInt(scanner.nextLine());
+        int number2 = Integer.parseInt(scanner.nextLine());
+        int number3 = Integer.parseInt(scanner.nextLine());
+
+        int add = operationAdd(number1, number2);
+        int result = operationSubtract(add, number3);
+        System.out.println(result);
+    }
+
+    private static int operationSubtract(int operationAdd, int number3) {
+        return operationAdd - number3;
+    }
+
+    private static int operationAdd(int number1, int number2) {
+        return number1 + number2;
+    }
+}
