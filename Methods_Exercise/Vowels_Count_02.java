9ce40c22df900251317fe3b78b371df33234dde7
Denis Buserski <81383938+DenisBuserski@users.noreply.github.com>
2022-05-10 07:57:29 +0300

    Add files via upload

---
+++ Methods_Exercise/Vowels_Count_02.java
@@ -0,0 +1,56 @@
+package MethodsExercise;
+
+import java.util.Scanner;
+
+public class VowelsCount {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String word = scanner.nextLine();
+
+        printVowels(word);
+    }
+
+    private static void printVowels(String word) {
+        //a e i o u
+        int vowelsSum = 0;
+        for (int i = 0; i < word.length(); i++) {
+            char letter = word.charAt(i);
+
+            switch (letter) {
+                case 97:
+                    vowelsSum++;
+                    break;
+                case 101:
+                    vowelsSum++;
+                    break;
+                case 105:
+                    vowelsSum++;
+                    break;
+                case 111:
+                    vowelsSum++;
+                    break;
+                case 117:
+                    vowelsSum++;
+                    break;
+                case 65:
+                    vowelsSum++;
+                    break;
+                case 69:
+                    vowelsSum++;
+                    break;
+                case 'I':
+                    vowelsSum++;
+                    break;
+                case 79:
+                    vowelsSum++;
+                    break;
+                case 85:
+                    vowelsSum++;
+                    break;
+            }
+
+        }
+        System.out.println(vowelsSum);
+    }
+}
