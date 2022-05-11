8659f0858519e3c0ded881e361ddfea7bc142116
Denis Buserski <81383938+DenisBuserski@users.noreply.github.com>
2022-05-11 07:46:35 +0300

    Update Factorial_Division_08.java

--- Methods_Exercise/Factorial_Division_08.java
+++ Methods_Exercise/Factorial_Division_08.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class FactorialDivision {
+public class Factorial_Division_08 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
