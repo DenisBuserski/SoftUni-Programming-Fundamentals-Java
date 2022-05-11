8b9a3335faf7584ff6656710788aac8bb7e4b799
Denis Buserski <81383938+DenisBuserski@users.noreply.github.com>
2022-05-11 07:44:01 +0300

    Add files via upload

---
+++ Methods_Exercise/NxN_Matrix_07.java
@@ -0,0 +1,18 @@
+package MethodsExercise;
+
+import java.util.Scanner;
+
+public class NxNMatrix {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = Integer.parseInt(scanner.nextLine());
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(n + " ");
+            }
+            System.out.println();
+        }
+    }
+}
