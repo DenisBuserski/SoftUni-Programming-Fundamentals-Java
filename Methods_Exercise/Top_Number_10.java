c3a16b994a4374f20af54ef88f84812e3cde02fd
Denis Buserski <81383938+DenisBuserski@users.noreply.github.com>
2022-05-12 22:22:29 +0300

    Add files via upload

---
+++ Methods_Exercise/Top_Number_10.java
@@ -0,0 +1,37 @@
+package MethodsExercise;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class TopNumber {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = Integer.parseInt(scanner.nextLine());
+        isTop(n);
+
+    }
+    private static void isTop(int n) {
+
+        for (int i = 1; i <= n; i++) {
+            int number = i;
+            int sum = 0;
+            boolean hasOdd = false;
+
+            while (number != 0) {
+                int currentDigit = number % 10;
+                sum += currentDigit;
+
+                if (currentDigit % 2 != 0) {
+                    hasOdd = true;
+                }
+                number /= 10;
+            }
+
+            if (sum % 8 == 0 && hasOdd) {
+                System.out.println(i);
+            }
+
+        }
+    }
+}
