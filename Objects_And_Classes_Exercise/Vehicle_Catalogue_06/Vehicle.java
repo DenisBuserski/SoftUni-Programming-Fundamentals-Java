48d44719e806789f8a42137f0c8f692af9afd3e3
Denis Buserski <81383938+DenisBuserski@users.noreply.github.com>
2022-05-14 12:06:45 +0300

    Update Vehicle.java

--- Objects_And_Classes_Exercise/Vehicle_Catalogue_06/Vehicle.java
+++ Objects_And_Classes_Exercise/Vehicle_Catalogue_06/Vehicle.java
@@ -1,7 +1,6 @@
-package ObjectsAndClassesExercise.VehicleCatalogue;
+package Vehicle_Catalogue_06;
 
 public class Vehicle {
-
         private String type;
         private String model;
         private String color;
@@ -44,6 +43,10 @@ public class Vehicle {
             return String.format("Type: %s\n" +
                     "Model: %s\n" +
                     "Color: %s\n" +
-                    "Horsepower: %d", getType().toUpperCase().charAt(0) + this.getType().substring(1), getModel(), getColor(), getHorsePower());
+                    "Horsepower: %d", 
+                                 getType().toUpperCase().charAt(0) + this.getType().substring(1), 
+                                 getModel(), getColor(), 
+                                 getHorsePower());
         }
+        
 }
