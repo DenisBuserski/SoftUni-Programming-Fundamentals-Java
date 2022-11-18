8af6c2720c967d4a1792176c3cc44510704c627a
Denis Buserski <denis.buserski@gmail.com>
2022-11-18 11:07:11 +0200

    Update and rename Arrays_Exercise/Zig_Zag_Arrays_03.java to Arrays/Exercise/Zig_Zag_Arrays_03.java

--- Arrays/Exercise/Zig_Zag_Arrays_03.java
+++ Arrays/Exercise/Zig_Zag_Arrays_03.java
@@ -5,21 +5,18 @@ public class Zig_Zag_Arrays_03 {
         Scanner scanner = new Scanner(System.in);
 
         int n = Integer.parseInt(scanner.nextLine());
-
         String[] line1 = new String[n];
         String[] line2 = new String[n];
 
         for (int i = 0; i < n; i++) {
             String[] array = scanner.nextLine().split(" ");
-
             String firstElement = array[0];
             String secondElement = array[1];
 
             if ((i + 1) % 2 == 0) {
                 line2[i] = firstElement;
                 line1[i] = secondElement;
-            }
-            else {
+            } else {
                 line1[i] = firstElement;
                 line2[i] = secondElement;
             }
