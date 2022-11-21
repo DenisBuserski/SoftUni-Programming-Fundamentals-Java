f1424182a0e76e69352c99ae0f7df385dd2f8f85
Denis Buserski <denis.buserski@gmail.com>
2022-11-21 06:44:32 +0200

    Update and rename Lists_Lab/Gauss_Trick_02.java to Lists/Lab/Gauss_Trick_02.java

--- Lists/Lab/Gauss_Trick_02.java
+++ Lists/Lab/Gauss_Trick_02.java
@@ -10,16 +10,17 @@ public class Gauss_Trick_02 {
         List<Integer> numbers = Arrays.stream(scanner.nextLine().split(" ")).map(Integer::parseInt).collect(Collectors.toList());
 
         for (int i = 0; i < numbers.size(); i++) {
-
             if (i == numbers.size() - 1) {
                 break;
             }
+            
             int currentElement = numbers.get(i);
             int oppositeElement = numbers.get((numbers.size()) - 1);
 
             numbers.set(i, currentElement + oppositeElement);
             numbers.remove(numbers.size() - 1);
         }
+        
         for (Integer number : numbers) {
             System.out.print(number + " ");
         }
