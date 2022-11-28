5c3d848ccd20b077f8b89ae63fe3feaa31ad1fce
Denis Buserski <denis.buserski@gmail.com>
2022-11-28 11:28:14 +0200

    Update Bit_At_Position_02.java

--- Bitwise_Operations/Bit_At_Position_02.java
+++ Bitwise_Operations/Bit_At_Position_02.java
@@ -3,12 +3,12 @@ import java.util.Scanner;
 public class Bit_At_Position_02 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        
         int input = Integer.parseInt(scanner.nextLine());
-        int mask = 1 << 1;
 
-        int result = (input & mask)>>1;
+        int position = 1;
+        int result = (input >> position) & 1;
+        
         System.out.println(result);
-
     }
 }
